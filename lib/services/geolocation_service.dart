379fff479a34855784f1f1a83760da8522d3fb63
Yoann Lafore <yoann.lafore@gmail.com>
2024-03-25 22:44:04 +0100

    fix(database): add GeolocatorPlatform injection to GeoLocationService

--- lib/services/geolocation_service.dart
+++ lib/services/geolocation_service.dart
@@ -1,37 +1,34 @@
+import "package:cloud_firestore/cloud_firestore.dart";
 import "package:geolocator/geolocator.dart";
 
 class GeoLocationService {
-  /// We do not perform dependency injection of [Geolocator] here because the
-  /// [GeoLocationService] methods cannot be used with an instance of [Geolocator]
+  final GeolocatorPlatform _geoLocator;
 
-  /// Also, this class is not a singleton on purpose. This is to allow for
-  /// dependency injection of the [GeoLocationService] in the repository that
-  /// uses it. This is useful for testing purposes.
+  // Settings to get the most accurate location
+  final LocationSettings locationSettings = AndroidSettings(
+    accuracy: LocationAccuracy.high,
+  );
 
-  // Source: Flutter Documentation (https://pub.dev/packages/geolocator)
-  // This code is adapted from the Flutter documentation
-  Future<Position> getCurrentPosition() async {
+  GeoLocationService({
+    required GeolocatorPlatform geoLocator,
+  }) : _geoLocator = geoLocator;
+
+  Future<GeoPoint> getCurrentPosition() async {
     bool serviceEnabled;
     LocationPermission permission;
 
     // Test if location services are enabled.
-    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    serviceEnabled = await _geoLocator.isLocationServiceEnabled();
     if (!serviceEnabled) {
-      // Location services are not enabled don't continue
-      // accessing the position and request users of the
-      // App to enable the location services.
       throw Exception("Location services are disabled.");
     }
 
-    permission = await Geolocator.checkPermission();
+    // Chech location permissions
+    permission = await _geoLocator.checkPermission();
     if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
+      permission = await _geoLocator.requestPermission();
       if (permission == LocationPermission.denied) {
-        // Permissions are denied, next time you could try
-        // requesting permissions again (this is also where
-        // Android's shouldShowRequestPermissionRationale
-        // returned true. According to Android guidelines
-        // your App should show an explanatory UI now.
+        // Permissions are denied after requesting
         throw Exception("Location permissions are denied");
       }
     }
@@ -43,8 +40,10 @@ class GeoLocationService {
       );
     }
 
-    // When we reach here, permissions are granted and we can
-    // continue accessing the position of the device.
-    return await Geolocator.getCurrentPosition();
+    final position = await _geoLocator.getCurrentPosition(
+      locationSettings: locationSettings,
+    );
+
+    return GeoPoint(position.latitude, position.longitude);
   }
 }
