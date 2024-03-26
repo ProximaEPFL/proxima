de518e6566cc04f56ac195b0ef27ec4a5e1aeaea
TheTexanCodeur <92913266+TheTexanCodeur@users.noreply.github.com>
2024-03-26 13:49:35 +0100

    feat(profile): widget consumes user data provider
    The widget can now access the data of the current user in order to display it.

--- lib/views/pages/profile_page.dart
+++ lib/views/pages/profile_page.dart
@@ -1,42 +1,49 @@
 import "package:flutter/material.dart";
 import "package:hooks_riverpod/hooks_riverpod.dart";
+import "package:proxima/viewmodels/profile_view_model.dart";
 
 class ProfilePage extends HookConsumerWidget {
   const ProfilePage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final asyncUserData = ref.watch(profileProvider);
+
     final theme = Theme.of(context);
 
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
-        title: UserInfos(theme: theme),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.settings),
-            onPressed: () {},
-          ),
-        ],
-      ),
-      body: Container(
-        color: theme.colorScheme.tertiaryContainer,
-        child: Center(
-          child: Column(
-            children: [
-              const SizedBox(height: 20),
-              CentauriPoints(theme: theme),
-              const SizedBox(height: 20),
+    return switch (asyncUserData) {
+      AsyncData(:final value) => Scaffold(
+          appBar: AppBar(
+            leading: IconButton(
+              icon: const Icon(Icons.arrow_back),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+            title: UserInfos(theme: theme, userEmail: value.user.email),
+            actions: [
+              IconButton(
+                icon: const Icon(Icons.settings),
+                onPressed: () {},
+              ),
             ],
           ),
+          body: Container(
+            color: theme.colorScheme.tertiaryContainer,
+            child: Center(
+              child: Column(
+                children: [
+                  const SizedBox(height: 20),
+                  CentauriPoints(theme: theme),
+                  const SizedBox(height: 20),
+                ],
+              ),
+            ),
+          ),
         ),
-      ),
-    );
+      AsyncError(:final error) => Text("Error: $error"),
+      _ => const Center(child: CircularProgressIndicator()),
+    };
   }
 }
 
@@ -65,6 +72,7 @@ class CentauriPoints extends HookConsumerWidget {
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
           const SizedBox(width: 5),
+          //TODO: get the centauri points from the viewmodel
           Text("Centauri Points:", style: theme.textTheme.headlineSmall),
         ],
       ),
@@ -72,27 +80,32 @@ class CentauriPoints extends HookConsumerWidget {
   }
 }
 
-class UserInfos extends HookConsumerWidget {
+class UserInfos extends StatelessWidget {
   const UserInfos({
     super.key,
     required this.theme,
+    required this.userEmail,
   });
 
   final ThemeData theme;
+  final String? userEmail;
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
     return Row(
       children: [
         const CircleAvatar(
           radius: 25,
+          //TODO: get the image from the viewmodel
           backgroundImage: NetworkImage("https://via.placeholder.com/150"),
         ),
         const SizedBox(width: 10),
         Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text("Profile", style: theme.textTheme.titleMedium),
+            //TODO: get the profile info from the viewmodel
+            Text(userEmail ?? "default@mail.com",
+                style: theme.textTheme.titleMedium),
             Text("User Profile", style: theme.textTheme.titleSmall),
           ],
         ),
