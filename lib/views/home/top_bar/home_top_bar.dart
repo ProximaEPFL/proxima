4dfa5c039c5ea7488797f6e3493f3391953aa536
CHOOSEIT <chooseitytt@gmail.com>
2024-03-26 12:05:23 +0100

    test(overview): readd logout button for testing

--- lib/views/home/top_bar/home_top_bar.dart
+++ lib/views/home/top_bar/home_top_bar.dart
@@ -1,12 +1,16 @@
 import "package:flutter/material.dart";
 import "package:hooks_riverpod/hooks_riverpod.dart";
+import "package:proxima/viewmodels/login_view_model.dart";
 import "package:proxima/views/filter_widgets/timeline_filter/timeline_filter_dropdown.dart";
+import "package:proxima/views/navigation/routes.dart";
 
 /*
   This widget is the top bar of the home page
   It contains the timeline filters and the user profile picture
 */
 class HomeTopBar extends HookConsumerWidget implements PreferredSizeWidget {
+  static const logoutButtonKey = Key("logout");
+
   static const homeTopBarKey = Key("homeTopBar");
   static const timelineFiltersDropDownKey = Key("timelineFiltersDropDown");
   static const profilePictureKey = Key("profilePicture");
@@ -15,17 +19,37 @@ class HomeTopBar extends HookConsumerWidget implements PreferredSizeWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return const Padding(
+    //Temporary logout
+    ref.listen(isUserLoggedInProvider, (_, isLoggedIn) {
+      if (!isLoggedIn) {
+        // Go to login page when the user is logged out
+        Navigator.pushNamedAndRemoveUntil(
+          context,
+          Routes.login.name,
+          (route) => false,
+        );
+      }
+    });
+
+    return Padding(
       key: homeTopBarKey,
-      padding: EdgeInsets.all(8.0),
+      padding: const EdgeInsets.all(8.0),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          TimeLineFiltersDropDown(
+          const TimeLineFiltersDropDown(
             key: timelineFiltersDropDownKey,
           ),
-          CircleAvatar(
+
+          //Temporary logout button
+          InkWell(
+            key: logoutButtonKey,
+            onTap: () => {ref.read(loginServiceProvider).signOut()},
+            child: const Icon(Icons.logout),
+          ),
+
+          const CircleAvatar(
             key: profilePictureKey,
             child: Text("PR"),
           ),
--- test/component/login_test.dart
+++ test/component/login_test.dart
@@ -2,6 +2,7 @@ import "package:firebase_core/firebase_core.dart";
 import "package:flutter_test/flutter_test.dart";
 import "package:hooks_riverpod/hooks_riverpod.dart";
 import "package:proxima/main.dart";
+import "package:proxima/views/home/top_bar/home_top_bar.dart";
 import "package:proxima/views/pages/home_page.dart";
 import "package:proxima/views/pages/login_page.dart";
 
@@ -38,5 +39,22 @@ void main() {
     // Check that pressing login redirects to the homepage
     final homePage = find.byType(HomePage);
     expect(homePage, findsOneWidget);
+  });
+
+  testWidgets("Login and Logout", (tester) async {
+    await tester.pumpWidget(mockedProxima);
+    await tester.pumpAndSettle();
+
+    final loginButton = find.byKey(LoginPage.loginButtonKey);
+    await tester.tap(loginButton);
+    await tester.pumpAndSettle();
+
+    final logoutButton = find.byKey(HomeTopBar.logoutButtonKey);
+    await tester.tap(logoutButton);
+    await tester.pumpAndSettle();
+
+    // Check that pressing logout redirects to the login page
+    final loginPage = find.byType(LoginPage);
+    expect(loginPage, findsOneWidget);
   });
 }
