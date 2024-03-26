402d2acc76dfa779f2efbeab0710af9e2b31be82
Camille Lannoye <101193378+camillelnne@users.noreply.github.com>
2024-03-26 14:02:24 +0100

    feat(ui-theme) : add skeleton of theme and google fonts dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,19 @@
 import "package:firebase_core/firebase_core.dart";
 import "package:flutter/material.dart";
+import "package:flutter/services.dart";
 import "package:hooks_riverpod/hooks_riverpod.dart";
 import "package:proxima/utils/firebase/firebase_options.dart";
 import "package:proxima/views/navigation/routes.dart";
+import 'package:google_fonts/google_fonts.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
 
   runApp(
     const ProviderScope(
@@ -26,6 +31,33 @@ class ProximaApp extends StatelessWidget {
       title: "Proxima",
       onGenerateRoute: generateRoute,
       initialRoute: Routes.login.name,
+      theme: ThemeData(
+        useMaterial3: true,
+
+        // Define the default brightness and colors.
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.indigo,
+          background: Colors.indigo.shade100,
+          brightness: Brightness.light,
+        ),
+
+        // Define the default `TextTheme`. Use this to specify the default
+        // text styling for headlines, titles, bodies of text, and more.
+        textTheme: TextTheme(
+          displayLarge: const TextStyle(
+            fontSize: 72,
+            fontWeight: FontWeight.bold,
+          ),
+          titleLarge: GoogleFonts.oswald(
+            // home page
+            fontSize: 30,
+            fontStyle: FontStyle.italic,
+          ),
+          bodyMedium: GoogleFonts.merriweather(),
+          displaySmall: GoogleFonts.pacifico(),
+          bodySmall: GoogleFonts.roboto(),
+        ),
+      ),
     );
   }
 }
