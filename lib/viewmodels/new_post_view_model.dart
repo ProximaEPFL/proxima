75868439fc1bfd24830a0440a27456c412b38b06
Alberts Reisons <alberts.reisons@epfl.ch>
2024-03-27 20:11:36 +0100

    fix(new post): removed unused view model
    The new post page is static for now, it will connect to the repository later.

--- lib/viewmodels/new_post_view_model.dart
+++
@@ -1,9 +0,0 @@
-import "package:hooks_riverpod/hooks_riverpod.dart";
-
-final titleProvider = Provider<String>((ref) {
-  return "New Post";
-});
-
-final bodyProvider = Provider<String>((ref) {
-  return "This is a new post.";
-});
