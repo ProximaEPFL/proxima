16e7e9756c7b4443d070089cee03daa527c92814
Alberts Reisons <alberts.reisons@epfl.ch>
2024-03-27 21:40:25 +0100

    fix(new post): optimise imports

--- lib/views/pages/new_post_page.dart
+++ lib/views/pages/new_post_page.dart
@@ -1,6 +1,4 @@
-import "package:flutter/cupertino.dart";
 import "package:flutter/material.dart";
-import "package:flutter/widgets.dart";
 import "package:hooks_riverpod/hooks_riverpod.dart";
 
 class NewPostPage extends HookConsumerWidget {
