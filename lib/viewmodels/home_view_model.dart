834e18a67549358b5675c8cad8253473ca68d59e
Yoann Lafore <yoann.lafore@gmail.com>
2024-04-07 00:26:09 +0200

    feat(overview): add posts provider for nearby posts

--- lib/viewmodels/home_view_model.dart
+++ lib/viewmodels/home_view_model.dart
@@ -1,7 +1,47 @@
 import "package:hooks_riverpod/hooks_riverpod.dart";
 import "package:proxima/models/ui/post_data.dart";
+import "package:proxima/services/database/post_repository_service.dart";
+import "package:proxima/services/database/user_repository_service.dart";
+import "package:proxima/services/geolocation_service.dart";
+
+const kmPostRadius = 0.1;
 
 // This provider is used to store the list of posts that are displayed in the home feed.
 final postList = Provider<List<Post>>((ref) {
   return List.empty();
+});
+
+final postProvider = FutureProvider<List<Post>>((ref) async {
+  final geoLocationService = ref.watch(geoLocationServiceProvider);
+  final postRepository = ref.watch(postRepositoryProvider);
+  final userRepository = ref.watch(userRepositoryProvider);
+
+  final position = await geoLocationService.getCurrentPosition();
+
+  final postsFirestore =
+      await postRepository.getNearPosts(position, kmPostRadius);
+
+  final postOwnersId = postsFirestore.map((post) => post.data.ownerId).toSet();
+
+  final postOwners = await Future.wait(
+    postOwnersId.map((userId) => userRepository.getUser(userId)),
+  );
+
+  final posts = postsFirestore.map((post) {
+    final owner = postOwners.firstWhere(
+      (user) => user.uid == post.data.ownerId,
+      orElse: () => throw Exception("Owner not found"),
+    );
+
+    return Post(
+      title: post.data.title,
+      description: post.data.description,
+      votes: post.data.voteScore,
+      posterUsername: owner.data.username,
+      commentNumber:
+          0, // TODO: Update appropriately when comments are implemented
+    );
+  }).toList();
+
+  return posts;
 });
