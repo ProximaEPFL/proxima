251e408ef524bb9d7499c02ce5d753fa2a893dc1
Yoann Lafore <yoann.lafore@gmail.com>
2024-04-07 22:11:01 +0200

    fix(overview): override the hash and ==
    This class represents the data of post displayed in the overview, thus it should be immutable and override the hash and == methods.:w

--- lib/models/ui/post_overview.dart
+++ lib/models/ui/post_overview.dart
@@ -1,3 +1,6 @@
+import "package:flutter/foundation.dart";
+
+@immutable
 class PostOverview {
   final String title;
   final String description;
@@ -12,4 +15,27 @@ class PostOverview {
     required this.commentNumber,
     required this.posterUsername,
   });
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is PostOverview &&
+        other.title == title &&
+        other.description == description &&
+        other.votes == votes &&
+        other.commentNumber == commentNumber &&
+        other.posterUsername == posterUsername;
+  }
+
+  @override
+  int get hashCode {
+    return Object.hash(
+      title,
+      description,
+      votes,
+      commentNumber,
+      posterUsername,
+    );
+  }
 }
