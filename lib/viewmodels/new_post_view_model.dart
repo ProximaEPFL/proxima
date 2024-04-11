61db9f1d3100f52a96c3263c210b335379a061ab
Alberts Reisons <alberts.reisons@epfl.ch>
2024-04-11 15:48:08 +0200

    refactor(new post vm): moved validation logic to the vm

--- lib/viewmodels/new_post_view_model.dart
+++ lib/viewmodels/new_post_view_model.dart
@@ -1,33 +1,101 @@
 import "package:cloud_firestore/cloud_firestore.dart";
+import "package:hooks_riverpod/hooks_riverpod.dart";
 import "package:proxima/models/database/post/post_data.dart";
-import "package:proxima/models/database/user/user_id_firestore.dart";
 import "package:proxima/services/database/post_repository_service.dart";
 import "package:proxima/services/geolocation_service.dart";
+import "package:proxima/viewmodels/login_view_model.dart";
 
-Future<void> addPost(
-  String title,
-  String description,
-  GeoLocationService geoLocationService,
-  UserIdFirestore? uid,
-  PostRepositoryService postRepositoryService,
-) async {
-  if (title.isEmpty || description.isEmpty) {
-    throw const FormatException("Title and description must not be empty");
+class NewPostState {
+  final String? titleError;
+  final String? descriptionError;
+  final bool posted;
+
+  NewPostState({
+    required this.titleError,
+    required this.descriptionError,
+    required this.posted,
+  });
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is NewPostState &&
+        other.titleError == titleError &&
+        other.descriptionError == descriptionError &&
+        other.posted == posted;
   }
 
-  final GeoPoint currPosition = await geoLocationService.getCurrentPosition();
-
-  if (uid == null) {
-    throw Exception("User must be logged in before creating a post");
+  @override
+  int get hashCode {
+    return Object.hash(titleError, descriptionError, posted);
   }
-
-  final PostData post = PostData(
-    ownerId: uid,
-    title: title,
-    description: description,
-    publicationTime: Timestamp.now(),
-    voteScore: 0,
-  );
-
-  await postRepositoryService.addPost(post, currPosition);
 }
+
+class NewPostViewModel extends AsyncNotifier<NewPostState> {
+  static const _titleError = "Please enter a title";
+  static const _bodyError = "Please enter a body";
+
+  @override
+  Future<NewPostState> build() async {
+    return NewPostState(
+      titleError: null,
+      descriptionError: null,
+      posted: false,
+    );
+  }
+
+  bool validate(String title, String description) {
+    if (title.isEmpty || description.isEmpty) {
+      state = AsyncData(
+        NewPostState(
+          titleError: title.isEmpty ? _titleError : null,
+          descriptionError: description.isEmpty ? _bodyError : null,
+          posted: false,
+        ),
+      );
+      return false;
+    }
+    return true;
+  }
+
+  Future<void> addPost(String title, String description) async {
+    state = const AsyncLoading();
+
+    final currentUser = ref.read(uidProvider);
+    if (currentUser == null) {
+      throw Exception("User must be logged in before creating a post");
+    }
+
+    if(!validate(title, description)) {
+      return;
+    }
+
+    final currPosition =
+        await ref.read(geoLocationServiceProvider).getCurrentPosition();
+    final postRepository = ref.read(postRepositoryProvider);
+
+    final post = PostData(
+      ownerId: currentUser,
+      title: title,
+      description: description,
+      publicationTime: Timestamp.now(),
+      voteScore: 0,
+    );
+
+    await postRepository.addPost(post, currPosition);
+
+    state = AsyncData(
+      NewPostState(
+        titleError: null,
+        descriptionError: null,
+        posted: true,
+      ),
+    );
+  }
+}
+
+final newPostStateProvider =
+    AsyncNotifierProvider<NewPostViewModel, NewPostState>(
+  () => NewPostViewModel(),
+);
