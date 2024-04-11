43a2584c0c830d277730a6a64b4fa8c4afbcbb07
gruvw <gruvw.dev@gmail.com>
2024-04-11 14:49:21 +0200

    ci: build step

--- lib/viewmodels/home_view_model_2.dart
+++
@@ -1,7 +0,0 @@
-import "package:hooks_riverpod/hooks_riverpod.dart";
-import "package:proxima/models/ui/post_data.dart";
-
-// This provider is used to store the list of posts that are displayed in the home feed.
-final postList = Provider<List<Post>>((ref) {
-  return List.empty();
-});
