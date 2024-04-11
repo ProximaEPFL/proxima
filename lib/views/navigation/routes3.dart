eeb064d538e04c64356c15673ad624932a6a7b98
gruvw <gruvw.dev@gmail.com>
2024-04-11 10:52:45 +0200

    feat(route): meaningless change

---
+++ lib/views/navigation/routes3.dart
@@ -0,0 +1,42 @@
+import "package:flutter/material.dart";
+import "package:proxima/views/pages/create_account_page.dart";
+import "package:proxima/views/pages/home/home_page.dart";
+import "package:proxima/views/pages/login/login_page.dart";
+import "package:proxima/views/pages/new_post/new_post_page.dart";
+import "package:proxima/views/pages/profile/profile_page.dart";
+
+enum Routes {
+  home("home"),
+  login("login"),
+  profile("profile"),
+  newPost("new post"),
+  createAccount("createAccount");
+
+  final String name;
+
+  const Routes(this.name);
+
+  static Routes parse(String name) {
+    return Routes.values.firstWhere((r) => r.name == name);
+  }
+
+  Widget page(Object? args) {
+    switch (this) {
+      case home:
+        return const HomePage();
+      case login:
+        return const LoginPage();
+      case profile:
+        return const ProfilePage();
+      case newPost:
+        return const NewPostPage();
+      case createAccount:
+        return const CreateAccountPage();
+    }
+  }
+}
+
+Route generateRoute(RouteSettings settings) {
+  final route = Routes.parse(settings.name!);
+  return MaterialPageRoute(builder: (_) => route.page(settings.arguments));
+}
