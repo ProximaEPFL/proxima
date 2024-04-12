7c164bc0a20f9042cc4a030284b005a9bd7c0289
CHOOSEIT <chooseitytt@gmail.com>
2024-04-12 15:14:59 +0200

    feat(profile-comment-popup): add comment popup

---
+++ lib/views/pages/profile/posts_info/popup/comment_popup.dart
@@ -0,0 +1,23 @@
+import "package:flutter/material.dart";
+
+class CommentPopUp extends StatelessWidget {
+  const CommentPopUp({
+    super.key,
+    required this.comment,
+  });
+
+  final String comment;
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      content: Text(comment),
+      actions: <Widget>[
+        IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.delete),
+        ),
+      ],
+    );
+  }
+}
