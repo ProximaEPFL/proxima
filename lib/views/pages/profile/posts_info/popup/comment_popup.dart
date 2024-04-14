2f90d4448d238780992f987c94fd05f0b679e480
CHOOSEIT <chooseitytt@gmail.com>
2024-04-14 17:35:00 +0200

    feat(profile-popup): adjust inner padding popup

--- lib/views/pages/profile/posts_info/popup/comment_popup.dart
+++ lib/views/pages/profile/posts_info/popup/comment_popup.dart
@@ -18,13 +18,25 @@ class CommentPopUp extends StatelessWidget {
         thumbVisibility: true,
         child: SingleChildScrollView(
           child: Padding(
-            padding: const EdgeInsets.all(8.0),
+            padding: const EdgeInsets.only(
+              top: 8.0,
+              right: 8.0,
+              bottom: 0.0,
+              left: 8.0,
+            ),
             child: Text(
               key: commentPopUpDescriptionKey,
               comment,
             ),
           ),
         ),
+      ),
+      contentPadding: const EdgeInsets.fromLTRB(24.0, 20.0, 24.0, 0.0),
+      actionsPadding: const EdgeInsets.only(
+        right: 24.0,
+        bottom: 12.0,
+        top: 0.0,
+        left: 24.0,
       ),
       actions: <Widget>[
         IconButton(
