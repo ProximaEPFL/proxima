b83b7ad7236c5356a77a3ec581592f1b0663249f
Joachim Favre <joachim@favre.xyz>
2024-04-16 23:41:16 +0200

    feat(UpvoteService): Add setUpvoteState function
    This is not done atomically for now

--- lib/services/database/post_upvote_service.dart
+++ lib/services/database/post_upvote_service.dart
@@ -1,4 +1,5 @@
 import "package:cloud_firestore/cloud_firestore.dart";
+import "package:proxima/models/database/post/post_data.dart";
 import "package:proxima/models/database/post/post_firestore.dart";
 import "package:proxima/models/database/post/post_id_firestore.dart";
 import "package:proxima/models/database/upvote_state.dart";
@@ -55,5 +56,74 @@ class PostUpvoteRepositoryService {
 
       return UpvoteState.none;
     });
+  }
+
+  Future<void> _runUpdateTransaction(
+    UserIdFirestore userId,
+    PostIdFirestore postId, {
+    required bool add,
+    required bool upvote,
+  }) async {
+    final votersCollection =
+        upvote ? _upvotersCollection(postId) : _downvotersCollection(postId);
+
+    // Note (add == upvote) is equivalent to (add XNOR upvote)
+    // If we want to add an upvote or remove a downvote, we add 1.
+    // IF we want to add a downvote or remove an upvote, we subtract 1.
+    final increment = (add == upvote) ? 1 : -1;
+
+    await firestore.runTransaction(
+      (transaction) async {
+        final postdocument = await transaction.get(_postDocument(postId));
+        final post = PostFirestore.fromDb(postdocument);
+        final voteScore = post.data.voteScore;
+
+        if (add) {
+          transaction.set(
+            votersCollection.doc(userId.value),
+            <String, dynamic>{},
+          );
+        } else {
+          transaction.delete(votersCollection.doc(userId.value));
+        }
+        transaction.update(
+          _postDocument(postId),
+          {
+            PostData.voteScoreField: voteScore + increment,
+          },
+        );
+      },
+    );
+  }
+
+  Future<void> setUpvoteState(
+    UserIdFirestore userId,
+    PostIdFirestore postId,
+    UpvoteState newState,
+  ) async {
+    // TODO: Everything should be done in a single transaction
+
+    final currState = await getUpvoteState(userId, postId);
+    if (currState == newState) return;
+
+    // Remove the current state, setting it to none.
+    if (currState != UpvoteState.none) {
+      await _runUpdateTransaction(
+        userId,
+        postId,
+        add: false,
+        upvote: currState == UpvoteState.upvoted,
+      );
+    }
+
+    // Apply the wanted state.
+    if (newState != UpvoteState.none) {
+      await _runUpdateTransaction(
+        userId,
+        postId,
+        add: true,
+        upvote: newState == UpvoteState.upvoted,
+      );
+    }
   }
 }
