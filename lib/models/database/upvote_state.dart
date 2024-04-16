453c63a4b736f47b3f9ef4e5768647fa464c665f
Joachim Favre <joachim@favre.xyz>
2024-04-17 00:55:23 +0200

    doc(UpvoteService): Add documentation for UpvoteState enum

--- lib/models/database/upvote_state.dart
+++ lib/models/database/upvote_state.dart
@@ -1,7 +1,11 @@
+/// Enum to represent the state of an upvote. A user can either
+/// be not voting, upvoting or downvoting on a post/comment.
 enum UpvoteState {
   none,
   upvoted,
   downvoted;
 
+  /// Returns the name of the enum value, without the `UpvoteState.` prefix.
+  /// For example, `UpvoteState.none.name` will return `"none"`
   String get name => toString().split(".").last;
 }
