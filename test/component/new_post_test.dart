e9834d85e6710f7da6029e99a10316c0d894b822
Camille Lannoye <101193378+camillelnne@users.noreply.github.com>
2024-04-16 13:02:39 +0200

    fix(clean-tests): remove unused import

--- test/component/new_post_test.dart
+++ test/component/new_post_test.dart
@@ -1,5 +1,4 @@
 import "package:cloud_firestore/cloud_firestore.dart";
-import "package:fake_cloud_firestore/fake_cloud_firestore.dart";
 import "package:firebase_core/firebase_core.dart";
 import "package:flutter/material.dart";
 import "package:flutter_test/flutter_test.dart";
