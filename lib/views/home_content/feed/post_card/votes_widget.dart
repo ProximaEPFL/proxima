aa9821f3e949545e306da2ce699015239f07bb65
Yoann Lafore <yoann.lafore@gmail.com>
2024-04-22 00:17:49 +0200

    feat(upvote): use the upvote viewmodel to get the voting logic

--- lib/views/home_content/feed/post_card/votes_widget.dart
+++ lib/views/home_content/feed/post_card/votes_widget.dart
@@ -1,29 +1,46 @@
 import "package:flutter/material.dart";
+import "package:hooks_riverpod/hooks_riverpod.dart";
+import "package:proxima/models/database/post/post_id_firestore.dart";
+import "package:proxima/models/database/vote/upvote_state.dart";
+import "package:proxima/viewmodels/upvote_view_model.dart";
 
 /// This widget is used to display the votes of a post.
 /// It contains two buttons to upvote and downvote the post and the number of votes.
-class VotesWidget extends StatelessWidget {
+class VotesWidget extends HookConsumerWidget {
   final int votes;
+  final PostIdFirestore postId;
 
   const VotesWidget({
     super.key,
     required this.votes,
+    required this.postId,
   });
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final asyncPostVote = ref.watch(upvoteStateProvider((postId: postId)));
+    final incrementToBase =
+        asyncPostVote.valueOrNull?.incrementToBaseState ?? 0;
+    final upvoteState =
+        asyncPostVote.valueOrNull?.upvoteState ?? UpvoteState.none;
+
+    final upvoteNotifier =
+        ref.read(upvoteStateProvider((postId: postId)).notifier);
+
     final upvote = IconButton(
       //Used to reduce the padding and InkWell created by the IconButton widget
       //https://stackoverflow.com/questions/50381157/how-do-i-remove-flutter-iconbutton-big-padding
       padding: EdgeInsets.zero,
       constraints: const BoxConstraints(),
-      icon: const Icon(Icons.expand_less, size: 30),
+      icon: Icon(
+        Icons.expand_less,
+        size: 30,
+        color: upvoteState == UpvoteState.upvoted ? Colors.blue : null,
+      ),
       style: const ButtonStyle(
         tapTargetSize: MaterialTapTargetSize.shrinkWrap,
       ),
-      onPressed: () {
-        //TODO: Implement the logic to upvote the post
-      },
+      onPressed: () => upvoteNotifier.triggerUpVote(),
     );
 
     final downvote = IconButton(
@@ -31,18 +48,20 @@ class VotesWidget extends StatelessWidget {
       //https://stackoverflow.com/questions/50381157/how-do-i-remove-flutter-iconbutton-big-padding
       padding: EdgeInsets.zero,
       constraints: const BoxConstraints(),
-      icon: const Icon(Icons.expand_more, size: 30),
+      icon: Icon(
+        Icons.expand_more,
+        size: 30,
+        color: upvoteState == UpvoteState.downvoted ? Colors.blue : null,
+      ),
       style: const ButtonStyle(
         tapTargetSize: MaterialTapTargetSize.shrinkWrap,
       ),
-      onPressed: () {
-        //TODO: Implement the logic to downvote the post
-      },
+      onPressed: () => upvoteNotifier.triggerDownVote(),
     );
 
     final voteCount = Padding(
       padding: const EdgeInsets.only(left: 8, right: 8),
-      child: Text(_votesString(votes)),
+      child: Text(_votesString(votes + incrementToBase)),
     );
 
     return Container(
