8cc2bb8c0185b4b65e899bac23c90668361e3f73
Alberts Reisons <alberts.reisons@epfl.ch>
2024-04-23 18:50:41 +0200

    feat(challenge-model): add a min radius + refactor

--- lib/services/database/challenge_repository_service.dart
+++ lib/services/database/challenge_repository_service.dart
@@ -1,9 +1,11 @@
 import "dart:core";
 
 import "package:cloud_firestore/cloud_firestore.dart";
+import "package:geoflutterfire_plus/geoflutterfire_plus.dart";
 import "package:hooks_riverpod/hooks_riverpod.dart";
 import "package:proxima/models/database/challenge/challenge_data.dart";
 import "package:proxima/models/database/challenge/challenge_firestore.dart";
+import "package:proxima/models/database/post/post_firestore.dart";
 import "package:proxima/models/database/post/post_id_firestore.dart";
 import "package:proxima/models/database/user/user_firestore.dart";
 import "package:proxima/models/database/user/user_id_firestore.dart";
@@ -17,28 +19,30 @@ class ChallengeRepositoryService {
   final GeoLocationService _geoLocationService;
 
   static const int _maxActiveChallenges = 3;
-  static const double _challengeRadius = 2000;
+  static const double _maxChallengeRadius = 3; // in km
+  static const double _minChallengeRadius = 0.5;
 
   ChallengeRepositoryService({
     required FirebaseFirestore firestore,
     required PostRepositoryService postRepositoryService,
     required GeoLocationService geoLocationService,
-  })
-      : _firestore = firestore,
+  })  : _firestore = firestore,
         _postRepositoryService = postRepositoryService,
         _geoLocationService = geoLocationService;
 
   Future<List<ChallengeFirestore>> getChallenges(UserIdFirestore uid) async {
     final userDocRef =
-    _firestore.collection(UserFirestore.collectionName).doc(uid.value);
+        _firestore.collection(UserFirestore.collectionName).doc(uid.value);
 
-    return _getChallenges(userDocRef, await nearbyUnsortedProvider());
+    return _getChallenges(userDocRef, await inRangeUnsortedPosts());
   }
 
-  Future<List<ChallengeFirestore>> _getChallenges(DocumentReference parentRef,
-      Iterator<PostIdFirestore> postIt,) async {
+  Future<List<ChallengeFirestore>> _getChallenges(
+    DocumentReference parentRef,
+    Iterable<PostIdFirestore> possiblePosts,
+  ) async {
     final challengesCollectionRef =
-    parentRef.collection(ChallengeFirestore.subCollectionName);
+        parentRef.collection(ChallengeFirestore.subCollectionName);
     final pastChallengesCollectionRef = parentRef
         .collection(ChallengeFirestore.pastChallengesSubCollectionName);
 
@@ -66,8 +70,9 @@ class ChallengeRepositoryService {
       final now = DateTime.now();
       final endOfDay = DateTime(now.year, now.month, now.day + 1);
 
+      final postIt = possiblePosts.iterator;
       while (
-      activeChallenges.length < _maxActiveChallenges && postIt.moveNext()) {
+          activeChallenges.length < _maxActiveChallenges && postIt.moveNext()) {
         final post = postIt.current;
 
         if (pastPostIds.contains(post.value)) {
@@ -90,17 +95,29 @@ class ChallengeRepositoryService {
     });
   }
 
-  Future<Iterator<PostIdFirestore>> nearbyUnsortedProvider() async {
+  Future<Iterable<PostIdFirestore>> inRangeUnsortedPosts() async {
     final position = await _geoLocationService.getCurrentPosition();
-    final possiblePosts = await _postRepositoryService.getNearPosts(
-      position, _challengeRadius,);
-    final possibleIds = possiblePosts.map((e) => e.id);
-    return possibleIds.iterator;
+    final geoFirePosition = GeoFirePoint(position);
+    List<PostFirestore> possiblePosts =
+        await _postRepositoryService.getNearPosts(
+      position,
+      _maxChallengeRadius,
+    );
+
+    possiblePosts.where(
+      (post) =>
+          geoFirePosition.distanceBetweenInKm(
+            geopoint: post.location.geoPoint,
+          ) >
+          _minChallengeRadius,
+    );
+
+    return possiblePosts.map((e) => e.id);
   }
 }
 
 final challengeRepositoryServiceProvider = Provider<ChallengeRepositoryService>(
-      (ref) {
+  (ref) {
     return ChallengeRepositoryService(
       firestore: ref.watch(firestoreProvider),
       postRepositoryService: ref.watch(postRepositoryProvider),
