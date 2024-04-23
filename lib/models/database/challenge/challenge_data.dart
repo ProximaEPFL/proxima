a4ba2dbee448f8d32f659f865c2b77d1bf9b5ccb
Alberts Reisons <alberts.reisons@epfl.ch>
2024-04-23 14:36:43 +0200

    feat(challenge-model): add expiry time to challenge data

--- lib/models/database/challenge/challenge_data.dart
+++ lib/models/database/challenge/challenge_data.dart
@@ -1,3 +1,4 @@
+import "package:cloud_firestore/cloud_firestore.dart";
 import "package:flutter/foundation.dart";
 
 @immutable
@@ -7,19 +8,25 @@ class ChallengeData {
   final bool isCompleted;
   static const String isCompletedField = "isCompleted";
 
+  final Timestamp expiresOn;
+  static const String expiresOnField = "expiresOn";
+
   const ChallengeData({
     required this.isCompleted,
+    required this.expiresOn,
   });
 
   factory ChallengeData.fromDb(Map<String, dynamic> data) {
     try {
       return ChallengeData(
         isCompleted: data[isCompletedField],
+        expiresOn: data[expiresOnField],
       );
     } catch (e) {
       if (e is TypeError) {
         throw FormatException(
-            "Cannot parse challenge document: ${e.toString()}");
+          "Cannot parse challenge document: ${e.toString()}",
+        );
       } else {
         rethrow;
       }
@@ -29,14 +36,17 @@ class ChallengeData {
   Map<String, dynamic> toDbData() {
     return {
       isCompletedField: isCompleted,
+      expiresOnField: expiresOn,
     };
   }
 
   @override
   bool operator ==(Object other) {
-    return other is ChallengeData && other.isCompleted == isCompleted;
+    return other is ChallengeData &&
+        other.isCompleted == isCompleted &&
+        other.expiresOn == expiresOn;
   }
 
   @override
-  int get hashCode => isCompleted.hashCode;
+  int get hashCode => Object.hash(isCompleted, expiresOn);
 }
