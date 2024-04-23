b9f42c9ae20fef0469d9b2afa0336cdddcf7fcdc
Alberts Reisons <alberts.reisons@epfl.ch>
2024-04-23 20:09:12 +0200

    feat(challenge-model): test getting new challenges

---
+++ test/services/database/challenge_repository_test.dart
@@ -0,0 +1,47 @@
+import "package:cloud_firestore/cloud_firestore.dart";
+import "package:fake_cloud_firestore/fake_cloud_firestore.dart";
+import "package:flutter_test/flutter_test.dart";
+import "package:proxima/services/database/challenge_repository_service.dart";
+import "package:proxima/services/database/post_repository_service.dart";
+
+import "../../mocks/data/mock_firestore_user.dart";
+import "../../mocks/data/mock_position.dart";
+import "../../mocks/data/mock_post_data.dart";
+
+void main() {
+  late FakeFirebaseFirestore firestore;
+  late PostRepositoryService postRepository;
+  late ChallengeRepositoryService challengeRepository;
+
+  setUp(() async {
+    firestore = FakeFirebaseFirestore();
+    postRepository = PostRepositoryService(firestore: firestore);
+    challengeRepository = ChallengeRepositoryService(
+      firestore: firestore,
+      postRepositoryService: postRepository,
+    );
+  });
+
+  group("ChallengeRepositoryService", () {
+    test("Get new challenges", () async {
+      const pos = userPosition0;
+      final fakePosts = PostDataGenerator.generatePostData(3);
+      final fakeUsers = FirestoreUserGenerator.generateUserFirestore(4);
+      for (final post in fakePosts) {
+        postRepository.addPost(post, pos);
+      }
+
+      final challenges =
+          await challengeRepository.getChallenges(fakeUsers[3].uid, pos);
+
+      expect(challenges.length, 3);
+      for (final challenge in challenges) {
+        expect(challenge.data.isCompleted, false);
+        expect(challenge.data.expiresOn.compareTo(Timestamp.now()), isPositive);
+
+        final actualPost = await postRepository.getPost(challenge.postId);
+        expect(fakePosts.contains(actualPost.data), true);
+      }
+    });
+  });
+}
