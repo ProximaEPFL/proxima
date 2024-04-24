6587023ce03873a49422a3b961b4d26626e9908c
Camille Lannoye <101193378+camillelnne@users.noreply.github.com>
2024-04-24 10:30:20 +0200

    refactor(tests): move emptyfirestoreuser and make it private

--- test/mocks/services/mock_user_repository_service.dart
+++ test/mocks/services/mock_user_repository_service.dart
@@ -1,17 +1,28 @@
+import "package:cloud_firestore/cloud_firestore.dart";
 import "package:mockito/mockito.dart";
 import "package:proxima/models/database/user/user_data.dart";
 import "package:proxima/models/database/user/user_firestore.dart";
 import "package:proxima/models/database/user/user_id_firestore.dart";
 import "package:proxima/services/database/user_repository_service.dart";
 
-import "../data/mock_firestore_user.dart";
-
 class MockUserRepositoryService extends Mock implements UserRepositoryService {
+  /// Not a coherent representation of a [UserFirestore]
+  /// This is just here as a placeholder value that will be overridden in the tests
+  final _emptyFirestoreUser = UserFirestore(
+    data: UserData(
+      displayName: "",
+      username: "",
+      joinTime: Timestamp.fromMillisecondsSinceEpoch(0),
+      centauriPoints: 0,
+    ),
+    uid: const UserIdFirestore(value: ""),
+  );
+
   @override
   Future<UserFirestore> getUser(UserIdFirestore? uid) {
     return super.noSuchMethod(
       Invocation.method(#getUser, [uid]),
-      returnValue: Future.value(emptyFirestoreUser),
+      returnValue: Future.value(_emptyFirestoreUser),
     );
   }
 
