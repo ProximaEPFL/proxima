591380570031f537757804d8d251e2653c52afaa
Joachim Favre <joachim@favre.xyz>
2024-04-24 22:36:36 +0200

    feat(challenge-page): add mock view-model

---
+++ lib/viewmodels/challenge_view_model.dart
@@ -0,0 +1,40 @@
+import "package:hooks_riverpod/hooks_riverpod.dart";
+import "package:proxima/models/ui/challenge_card_data.dart";
+
+class ChallengeViewModel extends AsyncNotifier<List<ChallengeCardData>> {
+  @override
+  Future<List<ChallengeCardData>> build() async {
+    return const [
+      ChallengeCardData(
+        title: "I hate UNIL, here's why",
+        distance: 700,
+        timeLeft: 27,
+        reward: 250,
+      ),
+      ChallengeCardData(
+        title:
+            "John fell here lmaoooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo",
+        distance: 3200,
+        timeLeft: 27,
+        reward: 400,
+      ),
+      ChallengeCardData(
+        title: "I'm moving out",
+        distance: 4000,
+        timeLeft: null,
+        reward: 350,
+      ),
+      ChallengeCardData(
+        title: "What a view!",
+        distance: null,
+        timeLeft: 27,
+        reward: 200,
+      ),
+    ];
+  }
+}
+
+final challengeProvider =
+    AsyncNotifierProvider<ChallengeViewModel, List<ChallengeCardData>>(
+  () => ChallengeViewModel(),
+);
