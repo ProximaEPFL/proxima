acd14c7bb50afbadd2cf23d4b5fc33fedf3b45fc
CHOOSEIT <chooseitytt@gmail.com>
2024-04-24 19:35:17 +0200

    test(comments): add a list of mock comments

---
+++ test/mocks/data/post_comment.dart
@@ -0,0 +1,29 @@
+import "package:proxima/models/ui/comment_post.dart";
+
+const List<CommentPost> testComments = [
+  CommentPost(
+    content:
+        "I totally agree! It's the little things in life that make all the difference.",
+    ownerDisplayName: "SunnySkyWalker",
+  ),
+  CommentPost(
+    content:
+        "Just what I needed to read today. Sometimes, simplicity is key to happiness.",
+    ownerDisplayName: "BookishBee",
+  ),
+  CommentPost(
+    content:
+        "Love this reminder! I try to start each day with a grateful heart.",
+    ownerDisplayName: "MilesOfSmiles",
+  ),
+  CommentPost(
+    content:
+        "Couldn't agree more! Walking my dog and enjoying nature always lifts my mood.",
+    ownerDisplayName: "SimpleJoys247",
+  ),
+  CommentPost(
+    content:
+        "This is so true. Just being outdoors makes me feel at peace and genuinely happy.",
+    ownerDisplayName: "HappyHiker42",
+  ),
+];
