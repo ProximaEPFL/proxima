cebe60fafbe588c251c77e2a16dbd9037cd61073
Joachim Favre <joachim@favre.xyz>
2024-04-25 20:21:56 +0200

    refactor(expect-rich-text): add matcher in argument to improve modularity

--- test/testutils/expect_rich_text.dart
+++ test/testutils/expect_rich_text.dart
@@ -1,7 +1,7 @@
 import "package:flutter/widgets.dart";
 import "package:flutter_test/flutter_test.dart";
 
-void expectOneRichText(String text) {
+void expectRichText(String text, Matcher matcher) {
   // The parameter findRichText from find.text does not appear to work
   // so I'm forced to use this uglier method.
   expect(
@@ -9,6 +9,6 @@ void expectOneRichText(String text) {
       (widget) =>
           widget is RichText && widget.text.toPlainText().contains(text),
     ),
-    findsOneWidget,
+    matcher,
   );
 }
