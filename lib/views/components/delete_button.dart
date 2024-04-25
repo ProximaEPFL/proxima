fd0f5d25302d4c4992059f100986eb92f107fa0c
gruvw <gruvw.dev@gmail.com>
2024-04-25 10:10:59 +0200

    feat(logout): create delete button accepting an async delete function

---
+++ lib/views/components/delete_button.dart
@@ -0,0 +1,37 @@
+import "package:flutter/material.dart";
+import "package:flutter_hooks/flutter_hooks.dart";
+import "package:hooks_riverpod/hooks_riverpod.dart";
+
+class DeleteButton extends HookConsumerWidget {
+  static const _deleteIconSize = 32.0;
+
+  final Future<void> Function() onClick;
+  final double iconSize;
+
+  const DeleteButton({
+    super.key,
+    required this.onClick,
+    this.iconSize = _deleteIconSize,
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final isLoading = useState(false);
+
+    if (isLoading.value) {
+      return const CircularProgressIndicator();
+    }
+
+    return IconButton(
+      icon: Icon(Icons.delete, size: iconSize),
+      onPressed: () async {
+        isLoading.value = true;
+        await onClick();
+
+        if (context.mounted) {
+          Navigator.pop(context);
+        }
+      },
+    );
+  }
+}
