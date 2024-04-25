d420850afdcb3ccec369589e7758b12e04e01cd0
Joachim Favre <joachim@favre.xyz>
2024-04-25 19:51:26 +0200

    style(challenge-page-test): remove unnecessary import

--- test/views/home_content/challenge_list_test.dart
+++ test/views/home_content/challenge_list_test.dart
@@ -1,4 +1,3 @@
-import "package:flutter/material.dart";
 import "package:flutter_test/flutter_test.dart";
 import "package:proxima/views/home_content/challenge/challenge_card.dart";
 import "package:proxima/views/home_content/challenge/challenge_list.dart";
