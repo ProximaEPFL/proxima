94ca8b16e95370e91d6021e9a77cdf408176aef0
Yoann Lafore <yoann.lafore@gmail.com>
2024-04-28 15:53:20 +0200

    feat(comment): add tests for comment data

---
+++ test/models/database/comment/comment_data_test.dart
@@ -0,0 +1,52 @@
+import "package:cloud_firestore/cloud_firestore.dart";
+import "package:flutter_test/flutter_test.dart";
+import "package:proxima/models/database/comment/comment_data.dart";
+
+import "../../../mocks/data/comment_data.dart";
+
+void main() {
+  group("Testing comment data", () {
+    test("hash overrides correctly", () {
+      final commentData = CommentDataGenerator.createRandomCommentData();
+
+      final expectedHash = Object.hash(
+        commentData.ownerId,
+        commentData.publicationTime,
+        commentData.voteScore,
+        commentData.content,
+      );
+
+      final actualHash = commentData.hashCode;
+
+      expect(actualHash, expectedHash);
+    });
+
+    test("equality overrides correctly", () {
+      final commentData = CommentDataGenerator.createRandomCommentData();
+
+      final otherCommentData = CommentData(
+        content: commentData.content,
+        ownerId: commentData.ownerId,
+        publicationTime: commentData.publicationTime,
+        voteScore: commentData.voteScore,
+      );
+
+      expect(commentData, otherCommentData);
+    });
+
+    test("fromDbData throw error when missing fields", () {
+      // The data is missing the voteScore field
+      final data = <String, dynamic>{
+        CommentData.contentField: "content",
+        CommentData.ownerIdField: "owner_id",
+        CommentData.publicationTimeField:
+            Timestamp.fromMillisecondsSinceEpoch(4564654),
+      };
+
+      expect(
+        () => CommentData.fromDbData(data),
+        throwsA(isA<FormatException>()),
+      );
+    });
+  });
+}
