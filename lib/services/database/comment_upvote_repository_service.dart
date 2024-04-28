7c0500944802bc18b480d062b9142ede75b71863
Yoann Lafore <yoann.lafore@gmail.com>
2024-04-28 22:28:38 +0200

    fix(comment): correct type returned by the provider

--- lib/services/database/comment_upvote_repository_service.dart
+++ lib/services/database/comment_upvote_repository_service.dart
@@ -1,13 +1,15 @@
 import "package:hooks_riverpod/hooks_riverpod.dart";
 import "package:proxima/models/database/comment/comment_data.dart";
 import "package:proxima/models/database/comment/comment_firestore.dart";
+import "package:proxima/models/database/comment/comment_id_firestore.dart";
 import "package:proxima/models/database/post/post_firestore.dart";
 import "package:proxima/models/database/post/post_id_firestore.dart";
 import "package:proxima/services/database/firestore_service.dart";
 import "package:proxima/services/database/upvote_repository_service.dart";
 
-final commentUpvoteRepositoryService =
-    Provider.family<UpvoteRepositoryService, PostIdFirestore>((ref, postId) {
+final commentUpvoteRepositoryService = Provider.family<
+    UpvoteRepositoryService<CommentIdFirestore>,
+    PostIdFirestore>((ref, postId) {
   final firestore = ref.read(firestoreProvider);
   final parentCollection = firestore
       .collection(PostFirestore.collectionName)
--- lib/services/database/post_upvote_repository_service.dart
+++ lib/services/database/post_upvote_repository_service.dart
@@ -1,10 +1,12 @@
 import "package:hooks_riverpod/hooks_riverpod.dart";
 import "package:proxima/models/database/post/post_data.dart";
 import "package:proxima/models/database/post/post_firestore.dart";
+import "package:proxima/models/database/post/post_id_firestore.dart";
 import "package:proxima/services/database/firestore_service.dart";
 import "package:proxima/services/database/upvote_repository_service.dart";
 
-final postUpvoteRepositoryProvider = Provider<UpvoteRepositoryService>((ref) {
+final postUpvoteRepositoryProvider =
+    Provider<UpvoteRepositoryService<PostIdFirestore>>((ref) {
   final firestore = ref.watch(firestoreProvider);
   return UpvoteRepositoryService(
     firestore: firestore,
