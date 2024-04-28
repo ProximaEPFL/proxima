f99e7657846cb17c9047249c286ae891a67ace36
Yoann Lafore <yoann.lafore@gmail.com>
2024-04-28 22:16:01 +0200

    feat(comment): add comment upvote repository provider

---
+++ lib/services/database/comment_upvote_repository_service.dart
@@ -0,0 +1,22 @@
+import "package:hooks_riverpod/hooks_riverpod.dart";
+import "package:proxima/models/database/comment/comment_data.dart";
+import "package:proxima/models/database/comment/comment_firestore.dart";
+import "package:proxima/models/database/post/post_firestore.dart";
+import "package:proxima/models/database/post/post_id_firestore.dart";
+import "package:proxima/services/database/firestore_service.dart";
+import "package:proxima/services/database/upvote_repository_service.dart";
+
+final commentUpvoteRepositoryService =
+    Provider.family<UpvoteRepositoryService, PostIdFirestore>((ref, postId) {
+  final firestore = ref.read(firestoreProvider);
+  final parentCollection = firestore
+      .collection(PostFirestore.collectionName)
+      .doc(postId.value)
+      .collection(CommentFirestore.subCollectionName);
+
+  return UpvoteRepositoryService(
+    firestore: firestore,
+    parentCollection: parentCollection,
+    voteScoreField: CommentData.voteScoreField,
+  );
+});
