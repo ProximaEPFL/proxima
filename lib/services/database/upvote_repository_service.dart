b1d559ec927a3cf27f412d58130d48d5590f018a
Yoann Lafore <yoann.lafore@gmail.com>
2024-04-28 22:13:09 +0200

    refactor(comment): create a base class for the upvoting repository logic

---
+++ lib/services/database/upvote_repository_service.dart
@@ -0,0 +1,100 @@
+import "package:cloud_firestore/cloud_firestore.dart";
+import "package:proxima/models/database/firestore/id_firestore.dart";
+import "package:proxima/models/database/user/user_id_firestore.dart";
+import "package:proxima/models/database/vote/upvote_state.dart";
+import "package:proxima/models/database/vote/vote_firestore.dart";
+
+/// This repository service is responsible for handling the upvotes of an
+/// arbitrary parent document.
+class UpvoteRepositoryService<ParentIdFirestore extends IdFirestore> {
+  final FirebaseFirestore _firestore;
+  final CollectionReference<Map<String, dynamic>> _parentCollection;
+  final String _voteScoreField;
+
+  UpvoteRepositoryService({
+    required FirebaseFirestore firestore,
+    required CollectionReference<Map<String, dynamic>> parentCollection,
+    required voteScoreField,
+  })  : _firestore = firestore,
+        _parentCollection = parentCollection,
+        _voteScoreField = voteScoreField;
+
+  /// Returns the document reference of the parent with id [parentId]
+  DocumentReference<Map<String, dynamic>> _parentDocument(
+    ParentIdFirestore parentId,
+  ) {
+    return _parentCollection.doc(parentId.value);
+  }
+
+  /// Returns the collection reference of the subcollection that contains the
+  /// list of users who voted the parent with id [parentId]
+  CollectionReference<Map<String, dynamic>> _votersCollection(
+    ParentIdFirestore parentId,
+  ) {
+    return _parentDocument(parentId)
+        .collection(VoteFirestore.votersSubCollectionName);
+  }
+
+  /// Returns the upvote state of the user with id [userId] on the parent with id [parentId]
+  /// This is done atomically, possibly as part of the transaction [transaction].
+  /// This only reads and writes nothing to the transaction, so it must be run before any
+  /// transaction write (see [FirebaseFirestore::runWithTransaction] documentation).
+  Future<UpvoteState> getUpvoteState(
+    UserIdFirestore userId,
+    ParentIdFirestore parentId, {
+    Transaction? transaction,
+  }) async {
+    final voteStateCollection = _votersCollection(parentId).doc(userId.value);
+    final voteState = transaction != null
+        ? await transaction.get(voteStateCollection)
+        : await voteStateCollection.get();
+
+    if (!voteState.exists) {
+      return UpvoteState.none;
+    } else {
+      return VoteFirestore.fromDbData(voteState.data()!).hasUpvoted
+          ? UpvoteState.upvoted
+          : UpvoteState.downvoted;
+    }
+  }
+
+  /// Sets the upvote state of the user with id [userId] on the parent with id [parentId]
+  /// to [newState]. This is done atomically.
+  Future<void> setUpvoteState(
+    UserIdFirestore userId,
+    ParentIdFirestore parentId,
+    UpvoteState newState,
+  ) async {
+    return await _firestore.runTransaction((transaction) async {
+      final currState = await getUpvoteState(
+        userId,
+        parentId,
+        transaction: transaction,
+      );
+      if (currState == newState) return;
+
+      int increment = 0;
+
+      // Remove the current state, setting it to none.
+      increment -= currState.increment;
+
+      // Apply the wanted state.
+      increment += newState.increment;
+
+      if (newState == UpvoteState.none) {
+        transaction.delete(_votersCollection(parentId).doc(userId.value));
+      } else {
+        transaction.set(
+          _votersCollection(parentId).doc(userId.value),
+          VoteFirestore(hasUpvoted: newState == UpvoteState.upvoted).toDbData(),
+        );
+      }
+
+      // Update the vote count
+      transaction.update(
+        _parentDocument(parentId),
+        {_voteScoreField: FieldValue.increment(increment)},
+      );
+    });
+  }
+}
