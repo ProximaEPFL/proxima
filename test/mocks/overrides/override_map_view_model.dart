0f838a1acb7fdbc60ac55363bc9476c02f42d2cd
TheTexanCodeur <92913266+TheTexanCodeur@users.noreply.github.com>
2024-04-29 20:29:20 +0200

    fix(map): made map view model more modular

--- test/mocks/overrides/override_map_view_model.dart
+++ test/mocks/overrides/override_map_view_model.dart
@@ -1,15 +1,21 @@
+import "package:flutter_test/flutter_test.dart";
 import "package:hooks_riverpod/hooks_riverpod.dart";
 import "package:proxima/viewmodels/map_view_model.dart";
 
 class MockMapViewModel extends AsyncNotifier<MapInfo> implements MapViewModel {
-  @override
-  Future<MapInfo> build() async {
-    throw Exception("Location services are disabled.");
-  }
+  final Future<MapInfo> Function() _build;
+  final Future<void> Function() _onRefresh;
+
+  MockMapViewModel({
+    Future<MapInfo> Function()? build,
+    Future<void> Function()? onRefresh,
+  })  : _build = build ??
+            (() async => throw Exception("Location services are disabled.")),
+        _onRefresh = onRefresh ?? (() async {});
 
   @override
-  Future<void> refresh() async {
-    state = const AsyncValue.loading();
-    state = await AsyncValue.guard(() => build());
-  }
+  Future<MapInfo> build() => _build();
+
+  @override
+  Future<void> refresh() => _onRefresh();
 }
