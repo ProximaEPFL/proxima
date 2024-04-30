5c80ee2faacf9227c343407f455150e2829d9aea
Joachim Favre <joachim@favre.xyz>
2024-04-30 18:01:30 +0200

    feat(sort-options): allow the user to choose the option on the ui

--- lib/views/home_content/feed/feed_sort_option_chips.dart
+++ lib/views/home_content/feed/feed_sort_option_chips.dart
@@ -1,19 +1,29 @@
 import "package:flutter/material.dart";
+import "package:hooks_riverpod/hooks_riverpod.dart";
 import "package:proxima/services/sorting/post_sort_option.dart";
+import "package:proxima/viewmodels/feed_sort_options_view_model.dart";
 
 /// This widget is the list of sort options for the timeline.
-class FeedSortOptionChips extends StatelessWidget {
+class FeedSortOptionChips extends ConsumerWidget {
   const FeedSortOptionChips({
     super.key,
   });
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final selectedOption = ref.watch(feedSortOptionsProvider);
+
     final sortOptions = PostSortOption.values.map((sortOption) {
       return ChoiceChip(
-        //TODO: Handle the sort option selection
-        selected: sortOption == PostSortOption.values[0],
+        selected: sortOption == selectedOption,
         label: Text(sortOption.name),
+        onSelected: (value) {
+          if (value) {
+            ref
+                .read(feedSortOptionsProvider.notifier)
+                .setSortOption(sortOption);
+          }
+        },
       );
     }).toList();
 
