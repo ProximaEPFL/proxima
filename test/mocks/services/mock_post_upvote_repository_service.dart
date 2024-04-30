fc3d58807ce6ffc47a082bc223c50dfbabafce5e
Alberts Reisons <alberts.reisons@epfl.ch>
2024-04-30 21:22:52 +0200

    refactor(mock-code-generation): rename main to empty

--- test/mocks/services/mock_post_upvote_repository_service.dart
+++ test/mocks/services/mock_post_upvote_repository_service.dart
@@ -2,4 +2,4 @@ import "package:mockito/annotations.dart";
 import "package:proxima/services/database/post_upvote_repository_service.dart";
 
 @GenerateNiceMocks([MockSpec<PostUpvoteRepositoryService>()])
-void main() {}
+void empty() {}
--- test/mocks/services/mock_user_repository_service.dart
+++ test/mocks/services/mock_user_repository_service.dart
@@ -2,4 +2,4 @@ import "package:mockito/annotations.dart";
 import "package:proxima/services/database/user_repository_service.dart";
 
 @GenerateNiceMocks([MockSpec<UserRepositoryService>()])
-void main() {}
+void empty() {}
