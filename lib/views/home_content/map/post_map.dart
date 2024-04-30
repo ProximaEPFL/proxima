f51d820f5efb7e879150521c32754000d32c4b4e
TheTexanCodeur <92913266+TheTexanCodeur@users.noreply.github.com>
2024-04-30 13:37:51 +0200

    fix(map): removed unused import

--- lib/views/home_content/map/post_map.dart
+++ lib/views/home_content/map/post_map.dart
@@ -1,4 +1,3 @@
-import "package:flutter/foundation.dart";
 import "package:flutter/material.dart";
 import "package:google_maps_flutter/google_maps_flutter.dart";
 import "package:hooks_riverpod/hooks_riverpod.dart";
