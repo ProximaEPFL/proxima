7cfc2612967b6f931c9ce937b9f9b63fb8134c55
gruvw <gruvw.dev@gmail.com>
2024-04-30 09:55:18 +0200

    feat(user-posts): add the refresh option to the `ProfileInfoColumn`

--- lib/views/pages/profile/info_cards/profile_info_column.dart
+++ lib/views/pages/profile/info_cards/profile_info_column.dart
@@ -1,25 +1,32 @@
 import "package:flutter/material.dart";
+import "package:proxima/utils/ui/types.dart";
 
 /// This widget defines the style of the columns in the profile page,
 /// such as the posts and comments columns
 class ProfileInfoColumn extends StatelessWidget {
+  static const _separator = SizedBox(height: 10);
+
   const ProfileInfoColumn({
     super.key,
     required this.itemList,
+    this.onRefresh,
   });
 
-  static const _separator = SizedBox(height: 10);
-
   final List<Widget> itemList;
+  final FutureVoidCallback? onRefresh;
 
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
+    final list = ListView.separated(
       padding: const EdgeInsets.all(8),
       scrollDirection: Axis.vertical,
       itemCount: itemList.length,
       itemBuilder: (BuildContext context, int index) => itemList[index],
       separatorBuilder: (BuildContext context, int index) => _separator,
     );
+
+    return onRefresh != null
+        ? RefreshIndicator(onRefresh: onRefresh!, child: list)
+        : list;
   }
 }
