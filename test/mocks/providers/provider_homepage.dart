2409eb7f931f50854f5ff2bbebbb2c3f1ce7befa
Joachim Favre <joachim@favre.xyz>
2024-05-01 14:48:58 +0200

    feat(home-page): add a provider for a mocked home page with a given fake firestore

--- test/mocks/providers/provider_homepage.dart
+++ test/mocks/providers/provider_homepage.dart
@@ -1,9 +1,14 @@
+import "package:fake_cloud_firestore/fake_cloud_firestore.dart";
 import "package:flutter/material.dart";
 import "package:hooks_riverpod/hooks_riverpod.dart";
+import "package:proxima/services/database/firestore_service.dart";
+import "package:proxima/services/geolocation_service.dart";
 import "package:proxima/views/navigation/routes.dart";
 import "package:proxima/views/pages/home/home_page.dart";
 
+import "../overrides/override_auth_providers.dart";
 import "../overrides/override_home_view_model.dart";
+import "../services/mock_geo_location_service.dart";
 
 const homePageApp = MaterialApp(
   onGenerateRoute: generateRoute,
@@ -24,3 +29,17 @@ final loadingHomePageProvider = ProviderScope(
   overrides: mockLoadingHomeViewModelOverride,
   child: homePageApp,
 );
+
+ProviderScope homePageFakeFirestoreProvider(
+  FakeFirebaseFirestore firestore,
+  MockGeoLocationService geoLocationService,
+) {
+  return ProviderScope(
+    overrides: [
+      ...loggedInUserOverrides,
+      firestoreProvider.overrideWithValue(firestore),
+      geoLocationServiceProvider.overrideWithValue(geoLocationService),
+    ],
+    child: homePageApp,
+  );
+}
