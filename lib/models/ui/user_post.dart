5126205779196c19800ae9010b1a48eeef703eab
Lucas Jung <gruvw.dev@gmail.com>
2024-05-01 10:17:12 +0200

    fix(user-posts): typo
    Co-authored-by: CHOOSEIT <53620532+CHOOSEIT@users.noreply.github.com>

--- lib/models/ui/user_post.dart
+++ lib/models/ui/user_post.dart
@@ -1,7 +1,7 @@
 import "package:flutter/foundation.dart";
 import "package:proxima/models/database/post/post_id_firestore.dart";
 
-/// A post that bellongs to the current logged in user.
+/// A post that belongs to the current logged in user.
 /// Used in their profile page.
 @immutable
 class UserPost {
