4afadd51a21239beb9eaf079ae6747889f8dcaea
TheTexanCodeur <92913266+TheTexanCodeur@users.noreply.github.com>
2024-05-02 22:37:58 +0200

    feat(map): documented MapInfo

--- lib/models/ui/map_info.dart
+++ lib/models/ui/map_info.dart
@@ -1,12 +1,17 @@
 import "package:google_maps_flutter/google_maps_flutter.dart";
 import "package:proxima/views/option_widgets/map/map_selection_option.dart";
 
+/// Information about how the map should be displayed.
 class MapInfo {
   const MapInfo({
     required this.initialLocation,
     required this.selectOption,
   });
+
+  /// The initial location of the map. Used to set the initial camera position
   final LatLng initialLocation;
+
+  /// The option selected by the user. Defines how the map will be displayed
   final MapSelectionOptions selectOption;
 
   @override
