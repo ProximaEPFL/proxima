8d1fbccae2ec13a67338df57ee79c615a240675b
Joachim Favre <joachim@favre.xyz>
2024-05-04 19:52:04 +0200

    feat(post-page): reload feed when popping a challenge view

--- lib/views/pages/post/post_page.dart
+++ lib/views/pages/post/post_page.dart
@@ -1,6 +1,7 @@
 import "package:flutter/material.dart";
 import "package:hooks_riverpod/hooks_riverpod.dart";
 import "package:proxima/models/ui/post_overview.dart";
+import "package:proxima/viewmodels/home_view_model.dart";
 import "package:proxima/viewmodels/post_view_model.dart";
 import "package:proxima/views/navigation/leading_back_button/leading_back_button.dart";
 import "package:proxima/views/pages/post/post_page_widget/bottom_bar_add_comment.dart";
@@ -51,34 +52,43 @@ class PostPage extends HookConsumerWidget {
       ),
     ];
 
-    return Scaffold(
-      appBar: AppBar(
-        leading: const LeadingBackButton(),
-        title: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: appBarContent,
-        ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.only(top: 8, bottom: 8, left: 8, right: 8),
-        child: Center(
-          child: ListView(
-            children: bodyChildren,
+    return PopScope(
+      onPopInvoked: (_) {
+        if (postOverview.isChallenge) {
+          // This is no longer a challenge, so refresh the feeed
+          ref.read(postOverviewProvider.notifier).refresh();
+        }
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          leading: const LeadingBackButton(),
+          title: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: appBarContent,
           ),
         ),
-      ),
-      persistentFooterButtons: [
-        Padding(
-          padding:
-              // This is necessary to prevent the keyboard from covering the bottom bar
-              EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
-          child: const BottomBarAddComment(
-            key: bottomBarAddCommentKey,
-            //TODO: Replace with actual username
-            currentDisplayName: "Username",
+        body: Padding(
+          padding: const EdgeInsets.only(top: 8, bottom: 8, left: 8, right: 8),
+          child: Center(
+            child: ListView(
+              children: bodyChildren,
+            ),
           ),
         ),
-      ],
+        persistentFooterButtons: [
+          Padding(
+            padding:
+                // This is necessary to prevent the keyboard from covering the bottom bar
+                EdgeInsets.only(
+                    bottom: MediaQuery.of(context).viewInsets.bottom),
+            child: const BottomBarAddComment(
+              key: bottomBarAddCommentKey,
+              //TODO: Replace with actual username
+              currentDisplayName: "Username",
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
