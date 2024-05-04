dc041da3d89efa7f5a93be656d69c382ac359969
Joachim Favre <joachim@favre.xyz>
2024-05-04 23:27:34 +0200

    feat: add a snackbar showing the number of Centauri won

---
+++ lib/utils/ui/centauri_snack_bar.dart
@@ -0,0 +1,19 @@
+import "package:flutter/material.dart";
+
+/// the state ScaffoldMessenger.of(context)
+/// Show a snackbar telling the user they won [value] Centauri points.
+/// [scaffoldMessengerState] is the current state of the scaffolds,
+/// on which to display the snackbar. It can typially be found
+/// using [ScaffoldMessenger.of(context)].
+void showCentauriPointsSnackBar(
+  int value,
+  ScaffoldMessengerState scaffoldMessengerState,
+) {
+  scaffoldMessengerState.showSnackBar(
+    SnackBar(
+      content: Text(
+        "You won $value Centauri! 🎉",
+      ),
+    ),
+  );
+}
