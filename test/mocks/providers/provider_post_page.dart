a272d0ff21beb0ef371d2d9767647ff154ec474f
Yoann Lafore <yoann.lafore@gmail.com>
2024-05-05 22:18:43 +0200

    test(comment): add mock provider for the post page

--- test/mocks/providers/provider_post_page.dart
+++ test/mocks/providers/provider_post_page.dart
@@ -1,10 +1,15 @@
 import "package:flutter/material.dart";
 import "package:hooks_riverpod/hooks_riverpod.dart";
+import "package:proxima/models/database/user/user_id_firestore.dart";
+import "package:proxima/services/database/comment_repository_service.dart";
+import "package:proxima/viewmodels/login_view_model.dart";
 import "package:proxima/views/navigation/routes.dart";
 import "package:proxima/views/pages/post/post_page.dart";
 
 import "../data/post_overview.dart";
 import "../overrides/override_comment_view_model.dart";
+import "../overrides/override_firestore.dart";
+import "../services/mock_comment_repository_service.dart";
 
 // Create a post page with the first post from the testPosts list
 final postPage = MaterialApp(
@@ -23,3 +28,18 @@ final nonEmptyPostPageProvider = ProviderScope(
   overrides: mockNonEmptyCommentViewModelOverride,
   child: postPage,
 );
+
+ProviderScope postPageProvider(
+  MockCommentRepositoryService commentRepository,
+  UserIdFirestore userId,
+) {
+  return ProviderScope(
+    overrides: [
+      ...firebaseMocksOverrides,
+      ...mockEmptyCommentViewModelOverride,
+      commentRepositoryProvider.overrideWithValue(commentRepository),
+      uidProvider.overrideWithValue(userId),
+    ],
+    child: postPage,
+  );
+}
