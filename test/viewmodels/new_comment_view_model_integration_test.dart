20cae24e2ef3db38ec88abc003bdeb6de244b195
Yoann Lafore <yoann.lafore@gmail.com>
2024-05-05 21:30:44 +0200

    test(comment): add tests for the new comment view model

---
+++ test/viewmodels/new_comment_view_model_integration_test.dart
@@ -0,0 +1,162 @@
+import "package:cloud_firestore/cloud_firestore.dart";
+import "package:fake_cloud_firestore/fake_cloud_firestore.dart";
+import "package:flutter_test/flutter_test.dart";
+import "package:hooks_riverpod/hooks_riverpod.dart";
+import "package:proxima/models/database/post/post_id_firestore.dart";
+import "package:proxima/models/database/user/user_id_firestore.dart";
+import "package:proxima/models/ui/new_comment_state.dart";
+import "package:proxima/services/database/comment_repository_service.dart";
+import "package:proxima/services/database/firestore_service.dart";
+import "package:proxima/viewmodels/login_view_model.dart";
+import "package:proxima/viewmodels/new_comment_view_model.dart";
+
+import "../mocks/data/comment_data.dart";
+import "../mocks/data/firestore_post.dart";
+import "../mocks/data/firestore_user.dart";
+import "../mocks/data/geopoint.dart";
+
+void main() {
+  group("Testing new comment view model", () {
+    late FakeFirebaseFirestore fakeFirestore;
+    late CommentDataGenerator commentDataGenerator;
+
+    late CommentRepositoryService commentRepository;
+    late AutoDisposeFamilyAsyncNotifierProvider<NewCommentViewModel,
+        NewCommentState, PostIdFirestore> newCommentViewModelProvider;
+
+    late ProviderContainer container;
+
+    late PostIdFirestore postId;
+    late UserIdFirestore userId;
+
+    setUp(() async {
+      fakeFirestore = FakeFirebaseFirestore();
+      commentDataGenerator = CommentDataGenerator();
+      userId = testingUserFirestoreId;
+
+      container = ProviderContainer(
+        overrides: [
+          firestoreProvider.overrideWithValue(fakeFirestore),
+          uidProvider.overrideWithValue(userId),
+        ],
+      );
+
+      commentRepository = container.read(commentRepositoryProvider);
+
+      // Add a post to the database on which the comments will be added
+      final post = FirestorePostGenerator().generatePostAt(userPosition0);
+      await setPostFirestore(post, fakeFirestore);
+      postId = post.id;
+
+      newCommentViewModelProvider = newCommentStateProvider(postId);
+    });
+
+    test("Add a valid comment and reset correctly", () async {
+      final validContent = commentDataGenerator.createMockCommentData().content;
+
+      final newCommentViewModel =
+          container.read(newCommentViewModelProvider.notifier);
+
+      //  --- Check the state before adding the comment
+      final stateBeforeAdd = await newCommentViewModel.future;
+      expect(
+        stateBeforeAdd,
+        NewCommentState(contentError: null, posted: false),
+      );
+
+      // --- Add the comment
+      final timeBeforeAdd = Timestamp.now();
+      final addResult = await newCommentViewModel.tryAddComment(validContent);
+      final timeAfterAdd = Timestamp.now();
+
+      expect(addResult, isTrue);
+
+      // --- Get the comments
+      final comments = await commentRepository.getComments(postId);
+
+      expect(comments, hasLength(1));
+
+      final comment = comments.first;
+
+      expect(comment.data.content, validContent);
+      expect(comment.data.ownerId, userId);
+
+      // --- Check the publication time is between the timeBeforeAdd and timeAfterAdd
+      expect(
+        comment.data.publicationTime.microsecondsSinceEpoch,
+        greaterThanOrEqualTo(timeBeforeAdd.microsecondsSinceEpoch),
+      );
+      expect(
+        comment.data.publicationTime.microsecondsSinceEpoch,
+        lessThanOrEqualTo(timeAfterAdd.microsecondsSinceEpoch),
+      );
+
+      // --- Check the state after adding the comment
+      final stateAfterAdd = await newCommentViewModel.future;
+      expect(
+        stateAfterAdd,
+        NewCommentState(contentError: null, posted: true),
+      );
+
+      // --- Reset the state
+      await newCommentViewModel.reset();
+
+      // --- Check the state after resetting
+      final stateAfterReset = await newCommentViewModel.future;
+      expect(
+        stateAfterReset,
+        NewCommentState(contentError: null, posted: false),
+      );
+    });
+
+    test("Add an empty comment exposes error message", () async {
+      const invalidContent = "";
+
+      final newCommentViewModel =
+          container.read(newCommentViewModelProvider.notifier);
+
+      //  --- Check the state before adding the comment
+      final stateBeforeAdd = await newCommentViewModel.future;
+      expect(
+        stateBeforeAdd,
+        NewCommentState(contentError: null, posted: false),
+      );
+
+      // --- Add the comment
+      final addResult = await newCommentViewModel.tryAddComment(invalidContent);
+
+      expect(addResult, isFalse);
+
+      // --- Check the state after adding the comment
+      final stateAfterAdd = await newCommentViewModel.future;
+      expect(
+        stateAfterAdd,
+        NewCommentState(
+          contentError: NewCommentViewModel.contentEmptyError,
+          posted: false,
+        ),
+      );
+    });
+
+    test("Add a comment without being logged in expose error async state",
+        () async {
+      final validContent = commentDataGenerator.createMockCommentData().content;
+
+      final newCommentViewModel =
+          container.read(newCommentViewModelProvider.notifier);
+
+      container.updateOverrides([
+        firestoreProvider.overrideWithValue(fakeFirestore),
+        uidProvider.overrideWithValue(null),
+      ]);
+
+      final addResult = await newCommentViewModel.tryAddComment(validContent);
+      expect(addResult, isFalse);
+
+      expect(
+        () async => await newCommentViewModel.future,
+        throwsA(isA<Exception>()),
+      );
+    });
+  });
+}
