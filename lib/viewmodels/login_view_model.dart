e58765ddd720d307a466e4c3de2d4883edb43bf4
Alberts Reisons <alberts.reisons@epfl.ch>
2024-05-06 08:23:56 +0200

    feat(challenge-logout): add a uid provider that is never null

--- lib/viewmodels/login_view_model.dart
+++ lib/viewmodels/login_view_model.dart
@@ -3,6 +3,7 @@ import "package:hooks_riverpod/hooks_riverpod.dart";
 import "package:proxima/models/database/user/user_id_firestore.dart";
 import "package:proxima/models/login_user.dart";
 import "package:proxima/services/login_service.dart";
+import "package:proxima/utils/ui/circular_value.dart";
 import "package:proxima/views/navigation/routes.dart";
 
 /// Firebase authentication change provider
@@ -26,6 +27,20 @@ final uidProvider = Provider<UserIdFirestore?>((ref) {
   final user = ref.watch(userProvider).valueOrNull;
 
   return user == null ? null : UserIdFirestore(value: user.id);
+});
+
+/// Firebase logged in user id provider, throws an exception if the user is
+/// not logged in
+final validUidProvider = Provider<UserIdFirestore>((ref) {
+  final user = ref.watch(uidProvider);
+
+  if (user == null) {
+    throw Exception(
+      "${CircularValue.debugErrorTag} User must be logged in.",
+    );
+  }
+
+  return user;
 });
 
 /// Login Service provider; dependency injection used for testing purposes
