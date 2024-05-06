4b5f64747f4bb4bea2551ec99d5e5911b054aff5
CHOOSEIT <chooseitytt@gmail.com>
2024-05-06 06:47:25 +0200

    feat(dynamic_user_avatar_view_model): remove code duplication

--- lib/viewmodels/dynamic_user_avatar_view_model.dart
+++ lib/viewmodels/dynamic_user_avatar_view_model.dart
@@ -1,6 +1,7 @@
 import "dart:async";
 
 import "package:hooks_riverpod/hooks_riverpod.dart";
+import "package:proxima/models/database/user/user_firestore.dart";
 import "package:proxima/models/database/user/user_id_firestore.dart";
 import "package:proxima/services/database/user_repository_service.dart";
 import "package:proxima/viewmodels/login_view_model.dart";
@@ -15,19 +16,19 @@ class DynamicUserAvatarViewModel
     final currentUID = ref.watch(uidProvider);
     final userDataBase = ref.watch(userRepositoryProvider);
 
+    late final UserFirestore userData;
+
     if (userID == null) {
       if (currentUID == null) {
         return Future.error("User id was not found.");
       }
 
-      final userData = await userDataBase.getUser(currentUID);
-
-      return userData.data.displayName;
+      userData = await userDataBase.getUser(currentUID);
     } else {
-      final userData = await userDataBase.getUser(userID);
-
-      return userData.data.displayName;
+      userData = await userDataBase.getUser(userID);
     }
+
+    return userData.data.displayName;
   }
 }
 
