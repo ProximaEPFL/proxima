3e35675409e2c23c129b131ee12df0241b3cb0b0
CHOOSEIT <chooseitytt@gmail.com>
2024-05-07 06:27:40 +0200

    test(dynamic_user_avatar_view_model): group tests setup

--- test/viewmodels/user_avatar_view_model_unit_test.dart
+++ test/viewmodels/user_avatar_view_model_unit_test.dart
@@ -10,12 +10,12 @@ import "package:proxima/viewmodels/login_view_model.dart";
 import "../mocks/data/firestore_user.dart";
 
 void main() {
-  group("User Avatar display name provider unit testing without current user",
-      () {
+  group("User Avatar ViewModel unit testing", () {
     late FakeFirebaseFirestore fakeFireStore;
     late UserRepositoryService userRepo;
 
     late ProviderContainer container;
+    late List<Override> overrideList;
 
     setUp(() async {
       fakeFireStore = FakeFirebaseFirestore();
@@ -24,68 +24,67 @@ void main() {
         firestore: fakeFireStore,
       );
 
-      container = ProviderContainer(
-        overrides: [
-          firestoreProvider.overrideWithValue(fakeFireStore),
-          userRepositoryProvider.overrideWithValue(userRepo),
-        ],
-      );
+      //Prepare the list of providers to override.
+      overrideList = [
+        firestoreProvider.overrideWithValue(fakeFireStore),
+        userRepositoryProvider.overrideWithValue(userRepo),
+      ];
     });
 
-    test("Find non existant current user display name", () async {
-      expect(
-        () async => await container
-            .read(dynamicUserAvatarViewModelProvider(null).future),
-        throwsA(isA<Exception>()),
-      );
+    group("User Avatar display name provider unit testing without current user",
+        () {
+      setUp(() async {
+        container = ProviderContainer(
+          overrides: overrideList,
+        );
+      });
+
+      test("Find non existant current user display name", () async {
+        expect(
+          () async => await container
+              .read(dynamicUserAvatarViewModelProvider(null).future),
+          throwsA(isA<Exception>()),
+        );
+      });
     });
-  });
-  group("User Avatar display name provider unit testing", () {
-    late FakeFirebaseFirestore fakeFireStore;
-    late UserRepositoryService userRepo;
+    group("User Avatar display name provider unit testing", () {
+      late List<UserFirestore> availableUsers;
 
-    late ProviderContainer container;
-    late List<UserFirestore> availableUsers;
+      setUp(() async {
+        //Define the available users
+        availableUsers = FirestoreUserGenerator.generateUserFirestore(10);
+        await setUsersFirestore(fakeFireStore, availableUsers);
 
-    setUp(() async {
-      fakeFireStore = FakeFirebaseFirestore();
+        container = ProviderContainer(
+          overrides: [
+            ...overrideList,
+            //Set current user id.
+            uidProvider.overrideWithValue(availableUsers[0].uid),
+          ],
+        );
+      });
 
-      userRepo = UserRepositoryService(
-        firestore: fakeFireStore,
-      );
-
-      availableUsers = FirestoreUserGenerator.generateUserFirestore(10);
-      await setUsersFirestore(fakeFireStore, availableUsers);
-
-      container = ProviderContainer(
-        overrides: [
-          firestoreProvider.overrideWithValue(fakeFireStore),
-          userRepositoryProvider.overrideWithValue(userRepo),
-          uidProvider.overrideWithValue(availableUsers[0].uid),
-        ],
-      );
-    });
-
-    test("Find current user display name", () async {
-      final displayName =
-          await container.read(dynamicUserAvatarViewModelProvider(null).future);
-
-      expect(
-        displayName,
-        availableUsers[0].data.displayName,
-      );
-    });
-
-    test("Find user display name by userId", () async {
-      for (final user in availableUsers) {
+      test("Find current user display name", () async {
         final displayName = await container
-            .read(dynamicUserAvatarViewModelProvider(user.uid).future);
+            .read(dynamicUserAvatarViewModelProvider(null).future);
 
         expect(
           displayName,
-          user.data.displayName,
+          availableUsers[0].data.displayName,
         );
-      }
+      });
+
+      test("Find user display name by userId", () async {
+        for (final user in availableUsers) {
+          final displayName = await container
+              .read(dynamicUserAvatarViewModelProvider(user.uid).future);
+
+          expect(
+            displayName,
+            user.data.displayName,
+          );
+        }
+      });
     });
   });
 }
