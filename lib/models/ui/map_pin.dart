10ea3644e0753a79089ccfccc4f441a0aeb5f1b4
TheTexanCodeur <92913266+TheTexanCodeur@users.noreply.github.com>
2024-05-08 09:25:48 +0200

    fix(map) : further comments in class MapPIn

--- lib/models/ui/map_pin.dart
+++ lib/models/ui/map_pin.dart
@@ -3,8 +3,12 @@ import "package:google_maps_flutter/google_maps_flutter.dart";
 //custom map pin class used to display pins on the map
 class MapPin {
   const MapPin({
+    //unique id that identifies the pin
+    //must be unique for each pin
     required this.id,
+    //position of the pin on the map
     required this.position,
+    //callback function that is called when we click on the pin
     required this.callbackFunction,
   });
 
