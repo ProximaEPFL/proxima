1ed0b9b876bda0d34ab21a6fd142ef91c021be5e
Alberts Reisons <alberts.reisons@epfl.ch>
2024-05-08 13:36:24 +0200

    test(challenge-logout): remove the regression test from end2end tests

--- test/end2end/app_test.dart
+++ test/end2end/app_test.dart
@@ -1,7 +1,6 @@
 import "package:cloud_firestore/cloud_firestore.dart";
 import "package:fake_cloud_firestore/fake_cloud_firestore.dart";
 import "package:firebase_core/firebase_core.dart";
-import "package:flutter/material.dart";
 import "package:flutter_test/flutter_test.dart";
 import "package:hooks_riverpod/hooks_riverpod.dart";
 import "package:mockito/mockito.dart";
@@ -18,7 +17,6 @@ import "package:proxima/views/pages/home/top_bar/app_top_bar.dart";
 import "package:proxima/views/pages/login/login_button.dart";
 import "package:proxima/views/pages/login/login_page.dart";
 import "package:proxima/views/pages/new_post/new_post_form.dart";
-import "package:proxima/views/pages/profile/components/logout_button.dart";
 import "package:proxima/views/pages/profile/info_cards/profile_info_card.dart";
 import "package:proxima/views/pages/profile/profile_data/profile_user_posts.dart";
 import "package:proxima/views/pages/profile/profile_page.dart";
@@ -74,31 +72,6 @@ void main() {
     await bottomNavigation(tester);
     await createPost(tester);
     await deletePost(tester);
-  });
-
-  testWidgets("Logout from challenges does not cause error", (tester) async {
-    await loadProxima(tester);
-
-    await loginToCreateAccount(tester);
-    await createAccountToHome(tester);
-
-    // Load challenges
-    await tester.tap(find.text("Challenge"));
-    await tester.pumpAndSettle();
-
-    // Go to profile page
-    final profilePicture = find.byKey(AppTopBar.profilePictureKey);
-    expect(profilePicture, findsOneWidget);
-    await tester.tap(profilePicture);
-    await tester.pumpAndSettle();
-
-    final logoutButton = find.byKey(LogoutButton.logoutButtonKey);
-    expect(logoutButton, findsOneWidget);
-    await tester.tap(logoutButton);
-    await tester.pumpAndSettle();
-
-    final errorPopup = find.bySubtype<AlertDialog>();
-    expect(errorPopup, findsNothing);
   });
 }
 
