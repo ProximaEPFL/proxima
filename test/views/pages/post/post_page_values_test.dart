6a07ef2e0cb3b13a46297f970bf02380dab8f2b2
gruvw <gruvw.dev@gmail.com>
2024-05-08 13:52:58 +0200

    test(time-dists): add custom post test for distance and setup time widget test

--- test/views/pages/post/post_page_values_test.dart
+++ test/views/pages/post/post_page_values_test.dart
@@ -1,5 +1,7 @@
 import "package:flutter_test/flutter_test.dart";
 import "package:hooks_riverpod/hooks_riverpod.dart";
+import "package:proxima/models/database/post/post_id_firestore.dart";
+import "package:proxima/models/ui/post_overview.dart";
 
 import "../../../mocks/data/post_overview.dart";
 import "../../../mocks/providers/provider_post_page.dart";
@@ -7,6 +9,16 @@ import "../../../mocks/services/setup_firebase_mocks.dart";
 
 void main() {
   late ProviderScope emptyPostPageWidget;
+  final customPost = PostOverview(
+    postId: const PostIdFirestore(value: "post_1"),
+    title: "title",
+    description: "description",
+    voteScore: 1,
+    commentNumber: 5,
+    ownerDisplayName: "owner",
+    publicationDate: DateTime.utc(1999),
+    distance: 100,
+  );
 
   setUp(() async {
     setupFirebaseAuthMocks();
@@ -21,6 +33,17 @@ void main() {
 
       final post = testPosts.first;
       final actualDistanceText = "${post.distance}m away";
+
+      final distanceDisplay = find.text(actualDistanceText);
+
+      expect(distanceDisplay, findsOneWidget);
+    });
+
+    testWidgets("Check correct distance on custom post", (tester) async {
+      await tester.pumpWidget(customPostOverviewPage(customPost));
+      await tester.pumpAndSettle();
+
+      final actualDistanceText = "${customPost.distance}m away";
 
       final distanceDisplay = find.text(actualDistanceText);
 
