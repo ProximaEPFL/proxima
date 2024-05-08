8fea55e99feb2c1ba35d7c1f37a1d1df4349a64e
Alberts Reisons <alberts.reisons@epfl.ch>
2024-05-08 13:35:38 +0200

    test(challenge-logout): create the regression test on the view-model

--- test/viewmodels/challenge_view_model_integration_test.dart
+++ test/viewmodels/challenge_view_model_integration_test.dart
@@ -6,6 +6,7 @@ import "package:proxima/services/database/challenge_repository_service.dart";
 import "package:proxima/services/database/firestore_service.dart";
 import "package:proxima/services/database/user_repository_service.dart";
 import "package:proxima/services/geolocation_service.dart";
+import "package:proxima/utils/ui/circular_value.dart";
 import "package:proxima/viewmodels/challenge_view_model.dart";
 import "package:proxima/viewmodels/login_view_model.dart";
 
@@ -17,160 +18,193 @@ import "../mocks/services/mock_geo_location_service.dart";
 
 void main() {
   late MockGeoLocationService geoLocationService;
-  late UserRepositoryService userRepo;
   late FakeFirebaseFirestore fakeFireStore;
   late ProviderContainer container;
 
-  setUp(() async {
+  setUp(() {
     geoLocationService = MockGeoLocationService();
     fakeFireStore = FakeFirebaseFirestore();
-
-    container = ProviderContainer(
-      overrides: [
-        geoLocationServiceProvider.overrideWithValue(geoLocationService),
-        uidProvider.overrideWithValue(testingUserFirestoreId),
-        firestoreProvider.overrideWithValue(fakeFireStore),
-      ],
-    );
-
-    userRepo = container.read(userRepositoryProvider);
-
     when(geoLocationService.getCurrentPosition()).thenAnswer(
       (_) async => userPosition1,
     );
   });
 
-  test("No challenges are returned when the database is empty", () async {
-    final challenges = await container.read(challengeProvider.future);
-    expect(challenges, isEmpty);
+  group("Normal use", () {
+    late UserRepositoryService userRepo;
+
+    setUp(() async {
+      container = ProviderContainer(
+        overrides: [
+          geoLocationServiceProvider.overrideWithValue(geoLocationService),
+          uidProvider.overrideWithValue(testingUserFirestoreId),
+          firestoreProvider.overrideWithValue(fakeFireStore),
+        ],
+      );
+
+      userRepo = container.read(userRepositoryProvider);
+    });
+
+    test("No challenges are returned when the database is empty", () async {
+      final challenges = await container.read(challengeProvider.future);
+      expect(challenges, isEmpty);
+    });
+
+    test(
+        "`ChallengeFirestore` is transformed correctly into `ChallengeCardData`",
+        () async {
+      const extraTime = Duration(hours: 2, minutes: 30);
+      final challengeGenerator = FirestoreChallengeGenerator();
+      final postGenerator = FirestorePostGenerator();
+
+      final post = postGenerator.generatePostAt(
+            userPosition1,
+          ); // the challenge is added by hand, so we can use the user position
+          await setPostFirestore(post, fakeFireStore);
+
+      final challenge = challengeGenerator.generateChallenge(false, extraTime);
+      await setChallenge(fakeFireStore, challenge, testingUserFirestoreId);
+
+      final challenges = await container.read(challengeProvider.future);
+          expect(challenges.length, 1);
+
+      final uiChallenge = challenges.first;
+      expect(uiChallenge.distance, 0);
+      expect(
+        uiChallenge.timeLeft,
+        2,
+      );
+      expect(uiChallenge.isFinished, false);
+      expect(
+          uiChallenge.reward, ChallengeRepositoryService.soloChallengeReward);
+      expect(uiChallenge.title, post.data.title);
+    });
+
+    test("Completed challenge is transformed correctly", () async {
+      const extraTime = Duration(hours: 2, minutes: 30);
+      final challengeGenerator = FirestoreChallengeGenerator();
+      final postGenerator = FirestorePostGenerator();
+
+      final post = postGenerator.generatePostAt(
+        userPosition1,
+      ); // the challenge is added by hand, so we can use the user position
+      await setPostFirestore(post, fakeFireStore);
+
+      final challenge = challengeGenerator.generateChallenge(true, extraTime);
+      await setChallenge(fakeFireStore, challenge, testingUserFirestoreId);
+
+      final challenges = await container.read(challengeProvider.future);
+      expect(challenges.length, 1);
+
+      final uiChallenge = challenges.first;
+      expect(uiChallenge.distance, null);
+      expect(
+        uiChallenge.timeLeft,
+        2,
+      );
+      expect(uiChallenge.isFinished, true);
+      expect(
+          uiChallenge.reward, ChallengeRepositoryService.soloChallengeReward);
+      expect(uiChallenge.title, post.data.title);
+    });
+
+    test("Challenges are sorted correctly", () async {
+      const extraTime = Duration(hours: 2, minutes: 30);
+      final challengeGenerator = FirestoreChallengeGenerator();
+      final postGenerator = FirestorePostGenerator();
+
+      final posts = postGenerator.generatePostsAt(userPosition1, 3);
+      setPostsFirestore(posts, fakeFireStore);
+
+      final finishedChallenges =
+          challengeGenerator.generateChallenges(2, true, extraTime);
+      final activeChallenges =
+          challengeGenerator.generateChallenges(1, false, extraTime);
+
+      await setChallenges(
+        fakeFireStore,
+        finishedChallenges,
+        testingUserFirestoreId,
+      );
+      await setChallenges(
+        fakeFireStore,
+        activeChallenges,
+        testingUserFirestoreId,
+      );
+
+      final challenges = await container.read(challengeProvider.future);
+      final areChallengesFinished =
+          challenges.map((c) => c.isFinished).toList();
+
+      expect(
+        areChallengesFinished,
+        List.filled(activeChallenges.length, false) +
+            List.filled(finishedChallenges.length, true),
+      );
+    });
+
+    test("Challenge can be completed", () async {
+      const extraTime = Duration(hours: 2, minutes: 30);
+      final challengeGenerator = FirestoreChallengeGenerator();
+      final postGenerator = FirestorePostGenerator();
+
+      await setUserFirestore(fakeFireStore, testingUserFirestore);
+
+      final post = postGenerator.generatePostAt(
+        userPosition1,
+      ); // the challenge is added by hand, so we can use the user position
+      await setPostFirestore(post, fakeFireStore);
+
+      final challenge = challengeGenerator.generateChallenge(false, extraTime);
+      await setChallenge(fakeFireStore, challenge, testingUserFirestoreId);
+
+      await container
+          .read(challengeProvider.notifier)
+          .completeChallenge(challenge.postId);
+
+      await Future.delayed(const Duration(milliseconds: 100));
+
+      final challenges = await container.read(challengeProvider.future);
+      expect(challenges.length, 1);
+
+      final uiChallenge = challenges.first;
+      expect(uiChallenge.distance, null);
+      expect(
+        uiChallenge.timeLeft,
+        2,
+      );
+      expect(uiChallenge.isFinished, true);
+      expect(
+          uiChallenge.reward, ChallengeRepositoryService.soloChallengeReward);
+      expect(uiChallenge.title, post.data.title);
+
+      // tests that points are added
+      final updatedUser = await userRepo.getUser(testingUserFirestoreId);
+      final points = updatedUser.data.centauriPoints;
+      expect(points, ChallengeRepositoryService.soloChallengeReward);
+    });
   });
 
-  test("`ChallengeFirestore` is transformed correctly into `ChallengeCardData`",
-      () async {
-    const extraTime = Duration(hours: 2, minutes: 30);
-    final challengeGenerator = FirestoreChallengeGenerator();
-    final postGenerator = FirestorePostGenerator();
+  group("No logged in user", () {
+    setUp(() async {
+      container = ProviderContainer(
+        overrides: [
+          geoLocationServiceProvider.overrideWithValue(geoLocationService),
+          uidProvider.overrideWithValue(null),
+          firestoreProvider.overrideWithValue(fakeFireStore),
+        ],
+      );
+    });
 
-    final post = postGenerator.generatePostAt(
-      userPosition1,
-    ); // the challenge is added by hand, so we can use the user position
-    await setPostFirestore(post, fakeFireStore);
-
-    final challenge = challengeGenerator.generateChallenge(false, extraTime);
-    await setChallenge(fakeFireStore, challenge, testingUserFirestoreId);
-
-    final challenges = await container.read(challengeProvider.future);
-    expect(challenges.length, 1);
-
-    final uiChallenge = challenges.first;
-    expect(uiChallenge.distance, 0);
-    expect(
-      uiChallenge.timeLeft,
-      2,
-    );
-    expect(uiChallenge.isFinished, false);
-    expect(uiChallenge.reward, ChallengeRepositoryService.soloChallengeReward);
-    expect(uiChallenge.title, post.data.title);
-  });
-
-  test("Completed challenge is transformed correctly", () async {
-    const extraTime = Duration(hours: 2, minutes: 30);
-    final challengeGenerator = FirestoreChallengeGenerator();
-    final postGenerator = FirestorePostGenerator();
-
-    final post = postGenerator.generatePostAt(
-      userPosition1,
-    ); // the challenge is added by hand, so we can use the user position
-    await setPostFirestore(post, fakeFireStore);
-
-    final challenge = challengeGenerator.generateChallenge(true, extraTime);
-    await setChallenge(fakeFireStore, challenge, testingUserFirestoreId);
-
-    final challenges = await container.read(challengeProvider.future);
-    expect(challenges.length, 1);
-
-    final uiChallenge = challenges.first;
-    expect(uiChallenge.distance, null);
-    expect(
-      uiChallenge.timeLeft,
-      2,
-    );
-    expect(uiChallenge.isFinished, true);
-    expect(uiChallenge.reward, ChallengeRepositoryService.soloChallengeReward);
-    expect(uiChallenge.title, post.data.title);
-  });
-
-  test("Challenges are sorted correctly", () async {
-    const extraTime = Duration(hours: 2, minutes: 30);
-    final challengeGenerator = FirestoreChallengeGenerator();
-    final postGenerator = FirestorePostGenerator();
-
-    final posts = postGenerator.generatePostsAt(userPosition1, 3);
-    setPostsFirestore(posts, fakeFireStore);
-
-    final finishedChallenges =
-        challengeGenerator.generateChallenges(2, true, extraTime);
-    final activeChallenges =
-        challengeGenerator.generateChallenges(1, false, extraTime);
-
-    await setChallenges(
-      fakeFireStore,
-      finishedChallenges,
-      testingUserFirestoreId,
-    );
-    await setChallenges(
-      fakeFireStore,
-      activeChallenges,
-      testingUserFirestoreId,
-    );
-
-    final challenges = await container.read(challengeProvider.future);
-    final areChallengesFinished = challenges.map((c) => c.isFinished).toList();
-
-    expect(
-      areChallengesFinished,
-      List.filled(activeChallenges.length, false) +
-          List.filled(finishedChallenges.length, true),
-    );
-  });
-
-  test("Challenge can be completed", () async {
-    const extraTime = Duration(hours: 2, minutes: 30);
-    final challengeGenerator = FirestoreChallengeGenerator();
-    final postGenerator = FirestorePostGenerator();
-
-    await setUserFirestore(fakeFireStore, testingUserFirestore);
-
-    final post = postGenerator.generatePostAt(
-      userPosition1,
-    ); // the challenge is added by hand, so we can use the user position
-    await setPostFirestore(post, fakeFireStore);
-
-    final challenge = challengeGenerator.generateChallenge(false, extraTime);
-    await setChallenge(fakeFireStore, challenge, testingUserFirestoreId);
-
-    await container
-        .read(challengeProvider.notifier)
-        .completeChallenge(challenge.postId);
-
-    await Future.delayed(const Duration(milliseconds: 100));
-
-    final challenges = await container.read(challengeProvider.future);
-    expect(challenges.length, 1);
-
-    final uiChallenge = challenges.first;
-    expect(uiChallenge.distance, null);
-    expect(
-      uiChallenge.timeLeft,
-      2,
-    );
-    expect(uiChallenge.isFinished, true);
-    expect(uiChallenge.reward, ChallengeRepositoryService.soloChallengeReward);
-    expect(uiChallenge.title, post.data.title);
-
-    // tests that points are added
-    final updatedUser = await userRepo.getUser(testingUserFirestoreId);
-    final points = updatedUser.data.centauriPoints;
-    expect(points, ChallengeRepositoryService.soloChallengeReward);
+    test("No user only throws debug error", () async {
+      expect(
+        () async {
+          await container.read(challengeProvider.future);
+        },
+        throwsA(
+          (exception) =>
+              exception.toString().contains(CircularValue.debugErrorTag),
+        ),
+      );
+    });
   });
 }
