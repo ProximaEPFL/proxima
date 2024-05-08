792d38f6f60df7311b0bfb26e2b4144d3fef8f2f
gruvw <gruvw.dev@gmail.com>
2024-05-08 08:53:07 +0200

    feat(time-service): implementation of time service interface API

--- lib/services/human_time_service.dart
+++ lib/services/human_time_service.dart
@@ -1,4 +1,6 @@
 import "package:hooks_riverpod/hooks_riverpod.dart";
+import "package:intl/intl.dart";
+import "package:timeago/timeago.dart" as timeago;
 
 /// A function that returns the current date/time as [DateTime]
 typedef CurrentDateTimeCallback = DateTime Function();
@@ -6,6 +8,11 @@ typedef CurrentDateTimeCallback = DateTime Function();
 /// Human Time Service is a service that uses dependency injection
 /// to provide related time as a human readable text given a [DateTime]
 class HumanTimeService {
+  static final _absoluteTimeFormatter = DateFormat("EEEE, MMMM d, yyyy HH:mm");
+  static const _relativeLocale = "en_short";
+  static const _agoSuffix = "ago";
+  static const _agoExceptions = ["now"];
+
   final CurrentDateTimeCallback currentDateTimeCallback;
 
   HumanTimeService({
@@ -13,12 +20,22 @@ class HumanTimeService {
   });
 
   /// Converts a [DateTime] to a human readable time ago string
-  /// Examples of format: "5m ago", "now", 2 days ago
-  String textTimeSince(DateTime dateTime) {}
+  /// Examples of format: "5m ago", "now", "2 days ago"
+  String textTimeSince(DateTime dateTime) {
+    final textTime = timeago.format(dateTime, locale: _relativeLocale);
+
+    if (_agoExceptions.contains(textTime)) {
+      return textTime;
+    }
+
+    return "$textTime $_agoSuffix";
+  }
 
   /// Converts a [DateTime] to a human readable date time string
-  /// Example of format: Friday, April 19, 2024 18:04
-  String textTime(DateTime dateTime) {}
+  /// Example of format: "Friday, April 19, 2024 18:04"
+  String textTimeAbsolute(DateTime dateTime) {
+    return _absoluteTimeFormatter.format(dateTime);
+  }
 }
 
 final currentDateTimeCallbackProvider =
