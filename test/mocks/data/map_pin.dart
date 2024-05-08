9376d902f11303f73ac399fddc3f9d6d21a2865e
TheTexanCodeur <92913266+TheTexanCodeur@users.noreply.github.com>
2024-05-08 09:38:59 +0200

    fix(map): moved MapPin test file under test/model/ui

--- test/mocks/data/map_pin.dart
+++ test/mocks/data/map_pin.dart
@@ -3,9 +3,29 @@ import "package:proxima/models/ui/map_pin.dart";
 
 //mock data to test the MapPin viewmodel
 List<MapPin> testPins = [
-  MapPin(id: "1", position: const LatLng(0, 0), callbackFunction: () {}),
-  MapPin(id: "2", position: const LatLng(1, 1), callbackFunction: () {}),
-  MapPin(id: "3", position: const LatLng(2, 2), callbackFunction: () {}),
-  MapPin(id: "4", position: const LatLng(3, 3), callbackFunction: () {}),
-  MapPin(id: "5", position: const LatLng(4, 4), callbackFunction: () {}),
+  MapPin(
+    id: const MarkerId("1"),
+    position: const LatLng(0, 0),
+    callbackFunction: () {},
+  ),
+  MapPin(
+    id: const MarkerId("2"),
+    position: const LatLng(1, 1),
+    callbackFunction: () {},
+  ),
+  MapPin(
+    id: const MarkerId("3"),
+    position: const LatLng(2, 2),
+    callbackFunction: () {},
+  ),
+  MapPin(
+    id: const MarkerId("4"),
+    position: const LatLng(3, 3),
+    callbackFunction: () {},
+  ),
+  MapPin(
+    id: const MarkerId("5"),
+    position: const LatLng(4, 4),
+    callbackFunction: () {},
+  ),
 ];
