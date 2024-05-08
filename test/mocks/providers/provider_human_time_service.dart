b0e5aa1fe1618b7207bfb3f42d42989444b8d326
gruvw <gruvw.dev@gmail.com>
2024-05-08 09:27:03 +0200

    test(time-dists): fake providers for constant time services

--- test/mocks/providers/provider_human_time_service.dart
+++ test/mocks/providers/provider_human_time_service.dart
@@ -1,5 +1,19 @@
+import "package:hooks_riverpod/hooks_riverpod.dart";
 import "package:proxima/services/human_time_service.dart";
 
 CurrentDateTimeCallback constantTimeCallback() {
   return () => DateTime.utc(2000);
 }
+
+final constantDateTimeCallbackProvider =
+    Provider<CurrentDateTimeCallback>((ref) {
+  return constantTimeCallback();
+});
+
+final constantHumanTimeServiceProvider = Provider<HumanTimeService>((ref) {
+  final currentDateTimeCallback = ref.watch(constantDateTimeCallbackProvider);
+
+  return HumanTimeService(
+    currentDateTimeCallback: currentDateTimeCallback,
+  );
+});
