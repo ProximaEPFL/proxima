aa15a430ec3a451afa50f4b10de168ff911b08ea
gruvw <gruvw.dev@gmail.com>
2024-05-08 08:40:54 +0200

    feat(time-service): add types and implementation for getting current DateTime

--- lib/services/human_time_service.dart
+++ lib/services/human_time_service.dart
@@ -1,8 +1,18 @@
 import "package:hooks_riverpod/hooks_riverpod.dart";
 
+/// A function that returns the current date/time as [DateTime]
+typedef CurrentDateTimeCallback = DateTime Function();
+
+/// Human Time Service is a service that uses dependency injection
+/// to provide related time as a human readable text given a [DateTime]
 class HumanTimeService {
   HumanTimeService();
 }
+
+final currentDateTimeCallbackProvider =
+    Provider<CurrentDateTimeCallback>((ref) {
+  return DateTime.now;
+});
 
 final humanTimeServiceProvider = Provider<HumanTimeService>((ref) {
   return HumanTimeService();
