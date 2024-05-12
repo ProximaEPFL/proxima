d0f627596cd90f39ee8c3d43bdb63aa6ed318472
CHOOSEIT <chooseitytt@gmail.com>
2024-05-12 11:30:46 +0200

    feat(map_markers_view_model): add map markers view model

---
+++ lib/viewmodels/map_markers_view_model.dart
@@ -0,0 +1,30 @@
+import "package:google_maps_flutter/google_maps_flutter.dart";
+import "package:hooks_riverpod/hooks_riverpod.dart";
+import "package:proxima/models/ui/map_pin_details.dart";
+
+// The view model to create and store the markers on the map
+class MapMarkersViewModel extends Notifier<void> {
+  @override
+  void build() {}
+
+  final Set<Marker> _markers = {};
+  // Getter for the markers
+  Set<Marker> get markers => _markers;
+
+  void redrawMarkers(List<MapPinDetails> pinList) {
+    _markers.clear();
+    for (final pin in pinList) {
+      _markers.add(
+        Marker(
+          markerId: pin.id,
+          position: pin.position,
+          onTap: pin.callbackFunction,
+        ),
+      );
+    }
+  }
+}
+
+final mapMarkersViewModelProvider = NotifierProvider<MapMarkersViewModel, void>(
+  () => MapMarkersViewModel(),
+);
