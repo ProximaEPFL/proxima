d1478986892525ba313366d0e822ea2deca109db
Yoann Lafore <yoann.lafore@gmail.com>
2024-05-12 14:04:33 +0200

    feat(comment): add mode to represent a ref to a user comment

---
+++ lib/models/database/user_comment/user_comment_id_firestore.dart
@@ -0,0 +1,16 @@
+import "package:proxima/models/database/firestore/id_firestore.dart";
+
+class UserCommentIdFirestore implements IdFirestore {
+  @override
+  final String value;
+
+  UserCommentIdFirestore({required this.value});
+
+  @override
+  bool operator ==(Object other) {
+    return other is UserCommentIdFirestore && other.value == value;
+  }
+
+  @override
+  int get hashCode => value.hashCode;
+}
