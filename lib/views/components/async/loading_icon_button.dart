b33a9bbfb5126f4137a4c0c3af8f9b90631ffeff
TheTexanCodeur <92913266+TheTexanCodeur@users.noreply.github.com>
2024-05-12 12:07:38 +0200

    fix(loading button): revert changes

--- lib/views/components/async/loading_icon_button.dart
+++ lib/views/components/async/loading_icon_button.dart
@@ -2,7 +2,7 @@ import "package:flutter/material.dart";
 import "package:flutter_hooks/flutter_hooks.dart";
 import "package:proxima/views/helpers/types.dart";
 
-enum LoadingIconButtonState {
+enum LoadingState {
   still,
   pending,
   completed,
