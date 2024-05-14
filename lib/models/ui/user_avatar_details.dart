0965a52fcc060e1dc6214030ece7a3e516b4f7e6
Joachim Favre <joachim@favre.xyz>
2024-05-14 19:34:00 +0200

    doc(user-avatar-details): add missing documentation

--- lib/models/ui/user_avatar_details.dart
+++ lib/models/ui/user_avatar_details.dart
@@ -1,10 +1,16 @@
 import "package:flutter/foundation.dart";
 
+/// A class that stores data for a user avatar UI.
 @immutable
 class UserAvatarDetails {
   final String displayName;
   final int? userCentauriPoints;
 
+  /// Creates a [UserAvatarDetails] object.
+  /// [displayName] is the user's display name, of which
+  /// the first letter is displayed on the avatar.
+  /// [userCentauriPoints] is the user's centauri points,
+  /// which is used to color the avatar background.
   const UserAvatarDetails({
     required this.displayName,
     required this.userCentauriPoints,
