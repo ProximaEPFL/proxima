19fa4c70158ce63172c7074b6e2fbe93b386a9fa
TheTexanCodeur <92913266+TheTexanCodeur@users.noreply.github.com>
2024-05-14 10:05:56 +0200

    feat(user comment viewmodel): implemented widget that will display the comments

---
+++ lib/views/pages/profile/components/profile_data/profile_user_comments.dart
@@ -0,0 +1,44 @@
+import "package:flutter/material.dart";
+import "package:hooks_riverpod/hooks_riverpod.dart";
+import "package:proxima/viewmodels/user_comments_view_model.dart";
+import "package:proxima/views/components/async/circular_value.dart";
+import "package:proxima/views/pages/profile/components/info_cards/profile_info_card.dart";
+import "package:proxima/views/pages/profile/components/info_cards/profile_info_column.dart";
+
+class ProfileUserComments extends ConsumerWidget {
+  static const commentsColumnKey = Key("commentColumn");
+
+  static const noCommentsInfoText = "You don't have any comments yet.";
+
+  const ProfileUserComments({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final userComments = ref.watch(userCommentsViewModelProvider);
+
+    return CircularValue(
+      value: userComments,
+      builder: (context, value) {
+        final comments = value
+            .map(
+              (p) => ProfileInfoCard(
+                content: p.description,
+                onDelete: () => ref
+                    .read(userCommentsViewModelProvider.notifier)
+                    .deleteComment(p.commentId),
+              ),
+            )
+            .toList();
+
+        return ProfileInfoColumn(
+          key: commentsColumnKey,
+          onRefresh: ref.read(userCommentsViewModelProvider.notifier).refresh,
+          itemList: comments,
+          emptyInfoText: noCommentsInfoText,
+        );
+      },
+    );
+  }
+}
