5312bc6676f3f26be109ee143c6e5fcb56c22122
gruvw <gruvw.dev@gmail.com>
2024-05-14 10:26:20 +0200

    feat(splash): introduce the splash screen handling in `main.dart`

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,27 @@
 import "package:firebase_core/firebase_core.dart";
 import "package:flutter/material.dart";
 import "package:flutter/services.dart";
+import "package:flutter_native_splash/flutter_native_splash.dart";
 import "package:hooks_riverpod/hooks_riverpod.dart";
 import "package:proxima/utils/firebase_options.dart";
 import "package:proxima/views/proxima_app.dart";
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+
+  // Preserve the splash screen for initialization
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
 
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  SystemChrome.setPreferredOrientations([
+  await SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
   ]);
+
+  // Hide splash screen
+  FlutterNativeSplash.remove();
 
   runApp(
     const ProviderScope(
