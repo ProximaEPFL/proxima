fa4bfabcc94c8170055b85227e850734a613e947
TheTexanCodeur <92913266+TheTexanCodeur@users.noreply.github.com>
2024-05-14 08:51:54 +0200

    fix(map pin viewmodel): added blank line before return

--- lib/viewmodels/map/map_pin_view_model.dart
+++ lib/viewmodels/map/map_pin_view_model.dart
@@ -23,6 +23,7 @@ class MapPinViewModel extends AsyncNotifier<List<MapPinDetails>> {
     );
     final pinList = nearPosts.map((post) {
       final postPosition = post.location.geoPoint;
+
       return MapPinDetails(
         id: MarkerId(post.id.value),
         position: LatLng(postPosition.latitude, postPosition.longitude),
