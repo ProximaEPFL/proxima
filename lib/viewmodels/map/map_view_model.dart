f616434f09c027659dd894fd78c005700b61d0ee
Camille Lannoye <101193378+camillelnne@users.noreply.github.com>
2024-05-16 01:03:56 +0200

    fix(mapViewModel): discard getter followUser

--- lib/viewmodels/map/map_view_model.dart
+++ lib/viewmodels/map/map_view_model.dart
@@ -63,8 +63,6 @@ class MapViewModel extends AutoDisposeAsyncNotifier<MapDetails> {
   // This boolean is used to determine if the camera should follow the user
   bool _followUser = true;
 
-  bool get followUser => _followUser;
-
   /// Enable the camera to follow the user
   void enableFollowUser() {
     _followUser = true;
