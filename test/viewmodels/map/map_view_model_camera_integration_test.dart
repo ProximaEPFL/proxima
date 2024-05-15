22df2f8ad57830a11e6c816d4c0712c12e1a6c64
Camille Lannoye <101193378+camillelnne@users.noreply.github.com>
2024-05-15 17:27:57 +0200

    fix(test): fix typo

--- test/viewmodels/map/map_view_model_camera_integration_test.dart
+++ test/viewmodels/map/map_view_model_camera_integration_test.dart
@@ -1,29 +1,44 @@
 import "package:cloud_firestore/cloud_firestore.dart";
 import "package:flutter_test/flutter_test.dart";
+import "package:geolocator/geolocator.dart";
 import "package:google_maps_flutter/google_maps_flutter.dart";
+import "package:hooks_riverpod/hooks_riverpod.dart";
 import "package:mockito/mockito.dart";
+import "package:proxima/services/sensors/geolocation_service.dart";
 import "package:proxima/viewmodels/map/map_view_model.dart";
 
 import "../../mocks/services/mock_geo_location_service.dart";
+import "../../mocks/services/mock_geolocator_platform.dart";
 import "../../mocks/services/mock_google_map_controller.dart";
 
 void main() {
   late MockGoogleMapController mockMapController;
-  late MockGeoLocationService mockGeoLocationService;
+  late MockGeolocatorPlatform mockGeolocator;
+  late GeolocationService geolocationService;
   late MapViewModel mapViewModel;
+  late ProviderContainer container;
 
   const GeoPoint initPosition = GeoPoint(1, 1);
   const GeoPoint finalPosition = GeoPoint(2, 2);
 
   setUp(() {
     mockMapController = MockGoogleMapController();
-    mockGeoLocationService = MockGeoLocationService();
+    mockGeolocator = MockGeolocatorPlatform();
+    geolocationService = GeolocationService(geoLocator: mockGeolocator);
     mapViewModel = MapViewModel();
 
     mapViewModel.onMapCreated(mockMapController);
 
-    when(mockGeoLocationService.getCurrentPosition())
-        .thenAnswer((_) async => initPosition);
+    when(mockGeolocator.isLocationServiceEnabled())
+        .thenAnswer((_) async => true);
+    when(mockGeolocator.checkPermission())
+        .thenAnswer((_) async => LocationPermission.always);
+
+    container = ProviderContainer(
+      overrides: [
+        geolocationServiceProvider.overrideWithValue(geolocationService),
+      ],
+    );
   });
 
   group("Camera", () {
@@ -32,8 +47,31 @@ void main() {
       expect(mapViewModel.mapController.isCompleted, true);
 
       // Check if the camera is following the user's live location
-      when(mockGeoLocationService.getCurrentPosition())
-          .thenAnswer((_) async => finalPosition);
+      // Set the initial position
+      when(
+        mockGeolocator.getCurrentPosition(
+          locationSettings: geoLocationService.locationSettings,
+        ),
+      ).thenAnswer(
+        (_) async => getSimplePosition(
+          postPositions[0].latitude,
+          postPositions[0].longitude,
+        ),
+      );
+
+      // Set the moving position
+      when(
+        mockGeolocator.getPositionStream(
+          locationSettings: geoLocationService.locationSettings,
+        ),
+      ).thenAnswer(
+        (_) => Stream.fromIterable(
+          postPositions.map(
+            (geoPoint) =>
+                getSimplePosition(geoPoint.latitude, geoPoint.longitude),
+          ),
+        ),
+      );
 
       // Check if the map controller calls animateCamera
       final newCameraPosition = CameraPosition(
