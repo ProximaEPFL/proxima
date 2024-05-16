912055f1e8715489585f9c88787512d4bdd30436
Yoann Lafore <yoann.lafore@epfl.ch>
2024-05-17 01:45:11 +0200

    test(comment): remove unused generator functions

--- test/mocks/data/firestore_user_comment.dart
+++ test/mocks/data/firestore_user_comment.dart
@@ -1,9 +1,6 @@
-import "package:flutter_test/flutter_test.dart";
 import "package:proxima/models/database/comment/comment_id_firestore.dart";
-import "package:proxima/models/database/user/user_id_firestore.dart";
 import "package:proxima/models/database/user_comment/user_comment_data.dart";
 import "package:proxima/models/database/user_comment/user_comment_firestore.dart";
-import "package:proxima/services/database/comment/user_comment_repository_service.dart";
 
 import "user_comment_data.dart";
 
@@ -14,33 +11,6 @@ class UserCommentFirestoreGenerator {
 
   UserCommentFirestoreGenerator({int seed = 0})
       : _userCommentDataGenerator = UserCommentDataGenerator(seed: seed);
-
-  /// Add a comment for the user with id [userId]
-  /// It will use the [userCommentRepository] to add the comment
-  /// It returns the added comment
-  Future<UserCommentFirestore> addComment(
-    UserIdFirestore userId,
-    UserCommentRepositoryService userCommentRepository,
-  ) async {
-    return (await addComments(1, userId, userCommentRepository)).first;
-  }
-
-  /// Add [number] of user comments for the user with id [userId] to the firestore database.
-  /// It will use the [userCommentRepository] to add the comments.
-  /// It will return the list of user comments that were added.
-  Future<List<UserCommentFirestore>> addComments(
-    int number,
-    UserIdFirestore userId,
-    UserCommentRepositoryService userCommentRepository,
-  ) async {
-    final userComments = List.generate(number, (_) => createMockUserComment());
-    await Future.wait(
-      userComments.map(
-        (comment) => userCommentRepository.addUserComment(userId, comment),
-      ),
-    );
-    return userComments;
-  }
 
   /// Create a random mock [UserCommentFirestore] instance.
   /// The [commentId] and [data] can be provided to avoid randomness.
