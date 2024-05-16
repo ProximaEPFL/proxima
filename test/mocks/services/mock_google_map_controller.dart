1440b99f9f541e2c2f3775336ba0401ce8d8ec33
Camille Lannoye <101193378+camillelnne@users.noreply.github.com>
2024-05-16 12:07:19 +0200

    test(mapviewmodel): test camera movement

--- test/mocks/services/mock_google_map_controller.dart
+++ test/mocks/services/mock_google_map_controller.dart
@@ -4,7 +4,7 @@ import "package:mockito/mockito.dart";
 /// This is a mock class for the [GoogleMapController] class
 class MockGoogleMapController extends Mock implements GoogleMapController {
   @override
-  Future<void> animateCamera(CameraUpdate cameraUpdate) {
+  Future<void> animateCamera(CameraUpdate? cameraUpdate) {
     return super.noSuchMethod(
       Invocation.method(#animateCamera, [cameraUpdate]),
       returnValue: Future.value(),
--- test/viewmodels/map/map_view_model_unit_test.dart
+++ test/viewmodels/map/map_view_model_unit_test.dart
@@ -1,5 +1,6 @@
 import "package:flutter_test/flutter_test.dart";
 import "package:google_maps_flutter/google_maps_flutter.dart";
+import "package:mockito/mockito.dart";
 import "package:proxima/viewmodels/map/map_view_model.dart";
 
 import "../../mocks/services/mock_google_map_controller.dart";
@@ -31,5 +32,27 @@ void main() {
 
     //check if the map controller is created
     expect(mapViewModel.mapController.isCompleted, true);
+  });
+
+  test("On Camera move", () async {
+    //create a new instance of the map view model
+    final mapViewModel = MapViewModel();
+
+    final mockGoogleMapController = MockGoogleMapController();
+
+    //create a new instance of the map controller
+    mapViewModel.onMapCreated(mockGoogleMapController);
+
+    //new position for the camera
+    const newLocation = LatLng(1, 1);
+
+    //move the camera
+    await mapViewModel.updateCamera(newLocation);
+
+    when(mockGoogleMapController.animateCamera(any)).thenAnswer((_) async {});
+
+    //check if the camera is moved to the new location
+    final newCameraPosition = CameraPosition(target: newLocation, zoom: 17);
+    verify(mockGoogleMapController.animateCamera(any)).called(1);
   });
 }
