3d13ce0169927d7558f1c270002125cc555320ba
Yoann Lafore <yoann.lafore@epfl.ch>
2024-05-16 23:12:10 +0200

    test(comment): add function to create mock comments for a user under multiple posts

--- test/mocks/data/firestore_comment.dart
+++ test/mocks/data/firestore_comment.dart
@@ -1,19 +1,25 @@
+import "package:cloud_firestore/cloud_firestore.dart";
 import "package:proxima/models/database/comment/comment_data.dart";
 import "package:proxima/models/database/comment/comment_firestore.dart";
 import "package:proxima/models/database/comment/comment_id_firestore.dart";
 import "package:proxima/models/database/post/post_id_firestore.dart";
+import "package:proxima/models/database/user/user_id_firestore.dart";
 import "package:proxima/models/database/user_comment/user_comment_data.dart";
 import "package:proxima/models/database/user_comment/user_comment_firestore.dart";
 import "package:proxima/services/database/comment/comment_repository_service.dart";
 
 import "comment_data.dart";
+import "firestore_post.dart";
+import "geopoint.dart";
 
 class CommentFirestoreGenerator {
   int _commentId = 0;
   final CommentDataGenerator _commentDataGenerator;
+  final FirestorePostGenerator _postGenerator;
 
   CommentFirestoreGenerator({int seed = 0})
-      : _commentDataGenerator = CommentDataGenerator(seed: seed);
+      : _commentDataGenerator = CommentDataGenerator(seed: seed),
+        _postGenerator = FirestorePostGenerator();
 
   Future<(List<CommentFirestore>, List<UserCommentFirestore>)> addComments(
     int number,
@@ -30,6 +36,38 @@ class CommentFirestoreGenerator {
 
       final userCommentData =
           UserCommentData(parentPostId: postId, content: commentData.content);
+      final userComment =
+          UserCommentFirestore(id: commentId, data: userCommentData);
+      userComments.add(userComment);
+    }
+
+    return (postComments, userComments);
+  }
+
+  Future<(List<CommentFirestore>, List<UserCommentFirestore>)>
+      addCommentsForUser(
+    int number,
+    UserIdFirestore userId,
+    CommentRepositoryService commentRepository,
+    FirebaseFirestore firestore,
+  ) async {
+    final postComments = <CommentFirestore>[];
+    final userComments = <UserCommentFirestore>[];
+
+    final posts =
+        await _postGenerator.addPosts(firestore, userPosition0, number);
+
+    for (final post in posts) {
+      final commentData =
+          _commentDataGenerator.createMockCommentData(ownerId: userId);
+
+      final commentId =
+          await commentRepository.addComment(post.id, commentData);
+
+      postComments.add(CommentFirestore(id: commentId, data: commentData));
+
+      final userCommentData =
+          UserCommentData(parentPostId: post.id, content: commentData.content);
       final userComment =
           UserCommentFirestore(id: commentId, data: userCommentData);
       userComments.add(userComment);
