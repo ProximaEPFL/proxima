d34df0c330b78e3f72ce66428b8433a3500cd89e
CHOOSEIT <chooseitytt@gmail.com>
2024-05-18 13:57:43 +0200

    feat(ranking): add navigation to empty ranking page

---
+++ lib/views/pages/home/content/ranking/ranking_widget.dart
@@ -0,0 +1,10 @@
+import "package:flutter/material.dart";
+
+class RankingWidget extends StatelessWidget {
+  const RankingWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text("Ranking"));
+  }
+}
