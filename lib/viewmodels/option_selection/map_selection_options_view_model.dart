234c11f553873fbdc65b6d83821b6812130ace75
Joachim Favre <joachim@favre.xyz>
2024-05-18 15:36:09 +0200

    feat(map-selection): add view-model for option selection

---
+++ lib/viewmodels/option_selection/map_selection_options_view_model.dart
@@ -0,0 +1,15 @@
+import "package:hooks_riverpod/hooks_riverpod.dart";
+import "package:proxima/viewmodels/option_selection/options_view_model.dart";
+import "package:proxima/views/components/options/map/map_selection_options.dart";
+
+class MapSelectionOptionsViewModel
+    extends OptionsViewModel<MapSelectionOptions> {
+  static const defaultMapOption = MapSelectionOptions.nearby;
+
+  MapSelectionOptionsViewModel() : super(defaultMapOption);
+}
+
+final mapSelectionOptionsViewModelProvider =
+    NotifierProvider<MapSelectionOptionsViewModel, MapSelectionOptions>(
+  () => MapSelectionOptionsViewModel(),
+);
