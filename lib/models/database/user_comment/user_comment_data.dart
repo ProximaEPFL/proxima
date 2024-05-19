62916f51e7d4b7e99fbdfd9ae50c0c468e35b4b1
Yoann Lafore <yoann.lafore@gmail.com>
2024-05-19 15:30:15 +0200

    feat(comment): add publication time field

--- lib/models/database/user_comment/user_comment_data.dart
+++ lib/models/database/user_comment/user_comment_data.dart
@@ -1,3 +1,4 @@
+import "package:cloud_firestore/cloud_firestore.dart";
 import "package:proxima/models/database/post/post_id_firestore.dart";
 
 /// This class represents the data required to display a user's comment
@@ -7,15 +8,19 @@ class UserCommentData {
   final PostIdFirestore parentPostId;
   static const String parentPostIdField = "parentPostId";
 
-  /// The reference contains the content of the comment to be displayed.
+  /// The reference contains the content and publication time of the comment to be displayed.
   /// This is to avoid having to fetch the comment data from the comment
   /// collection under the post every time we want to display the user's comments.
   final String content;
   static const String contentField = "content";
 
+  final Timestamp publicationTime;
+  static const String publicationTimeField = "publicationTime";
+
   const UserCommentData({
     required this.parentPostId,
     required this.content,
+    required this.publicationTime,
   });
 
   /// This method will create an instance of [UserCommentData] from the
@@ -25,6 +30,7 @@ class UserCommentData {
       return UserCommentData(
         parentPostId: PostIdFirestore(value: data[parentPostIdField]),
         content: data[contentField],
+        publicationTime: data[publicationTimeField],
       );
     } catch (e) {
       if (e is TypeError) {
@@ -43,6 +49,7 @@ class UserCommentData {
     return {
       parentPostIdField: parentPostId.value,
       contentField: content,
+      publicationTimeField: publicationTime,
     };
   }
 
@@ -50,9 +57,10 @@ class UserCommentData {
   bool operator ==(Object other) {
     return other is UserCommentData &&
         other.parentPostId == parentPostId &&
-        other.content == content;
+        other.content == content &&
+        other.publicationTime == publicationTime;
   }
 
   @override
-  int get hashCode => Object.hash(parentPostId, content);
+  int get hashCode => Object.hash(parentPostId, content, publicationTime);
 }
