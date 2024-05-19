ecc7b85c0f6a932f5903cdceffd3d8ba20789935
gruvw <gruvw.dev@gmail.com>
2024-05-19 11:19:55 +0200

    test(ranknig-vm): expect correct number of users returned

--- test/viewmodels/users_ranking_view_model_test.dart
+++ test/viewmodels/users_ranking_view_model_test.dart
@@ -1,6 +1,7 @@
 import "package:fake_cloud_firestore/fake_cloud_firestore.dart";
 import "package:flutter_test/flutter_test.dart";
 import "package:hooks_riverpod/hooks_riverpod.dart";
+import "package:proxima/viewmodels/users_ranking_view_model.dart";
 
 import "../mocks/data/firestore_user.dart";
 import "../mocks/providers/provider_ranking.dart";
@@ -12,9 +13,22 @@ void main() {
 
     setUp(() async {
       fakeFirestore = FakeFirebaseFirestore();
-      await FirestoreUserGenerator.addUsers(fakeFirestore, 200);
+      await FirestoreUserGenerator.addUsers(
+        fakeFirestore,
+        UsersRankingViewModel.rankingLimit * 2,
+      );
 
       rankingContainer = rankingProviderContainer(fakeFirestore);
+    });
+
+    test("Correct number of users returned", () async {
+      final users =
+          await rankingContainer.read(usersRankingViewModelProvider.future);
+
+      expect(
+        users.rankElementDetailsList.length,
+        UsersRankingViewModel.rankingLimit,
+      );
     });
   });
 }
