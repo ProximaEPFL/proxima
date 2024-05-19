d6c1a0c094b40c0aa336bddadc97d8d1b867fc0d
Yoann Lafore <yoann.lafore@gmail.com>
2024-05-19 15:49:39 +0200

    Revert "feat(profile): update hash and == test of user posts"
    This reverts commit af4b29cc5672808f36860096f6753c691ae72b75.

--- test/models/ui/user_post_details_test.dart
+++ test/models/ui/user_post_details_test.dart
@@ -4,25 +4,17 @@ import "package:proxima/models/ui/user_post_details.dart";
 
 void main() {
   group("User Post model testing", () {
-    late DateTime publicationTime;
-
-    setUp(() {
-      publicationTime = DateTime.fromMillisecondsSinceEpoch(10000000);
-    });
-
     test("hash overrides correctly", () {
-      final userPost = UserPostDetails(
-        postId: const PostIdFirestore(value: "post_1"),
+      const userPost = UserPostDetails(
+        postId: PostIdFirestore(value: "post_1"),
         title: "title",
         description: "description",
-        publicationTime: publicationTime,
       );
 
       final expectedHash = Object.hash(
         userPost.postId,
         userPost.title,
         userPost.description,
-        userPost.publicationTime,
       );
 
       final actualHash = userPost.hashCode;
@@ -31,36 +23,32 @@ void main() {
     });
 
     test("equality overrides correctly", () {
-      final userPost = UserPostDetails(
-        postId: const PostIdFirestore(value: "post_1"),
+      const userPost = UserPostDetails(
+        postId: PostIdFirestore(value: "post_1"),
         title: "title",
         description: "description",
-        publicationTime: publicationTime,
       );
 
-      final userPostCopy = UserPostDetails(
-        postId: const PostIdFirestore(value: "post_1"),
+      const userPostCopy = UserPostDetails(
+        postId: PostIdFirestore(value: "post_1"),
         title: "title",
         description: "description",
-        publicationTime: publicationTime,
       );
 
       expect(userPost, userPostCopy);
     });
 
     test("inequality test on content", () {
-      final userPost1 = UserPostDetails(
-        postId: const PostIdFirestore(value: "post_1"),
+      const userPost1 = UserPostDetails(
+        postId: PostIdFirestore(value: "post_1"),
         title: "title",
         description: "description 1",
-        publicationTime: publicationTime,
       );
 
-      final userPost2 = UserPostDetails(
-        postId: const PostIdFirestore(value: "post_1"),
+      const userPost2 = UserPostDetails(
+        postId: PostIdFirestore(value: "post_1"),
         title: "title",
         description: "description 2",
-        publicationTime: publicationTime,
       );
 
       expect(userPost1 != userPost2, true);
