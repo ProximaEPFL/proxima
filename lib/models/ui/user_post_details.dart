5905c714df7714f572302970cdfdd2ea67afc41c
Yoann Lafore <yoann.lafore@gmail.com>
2024-05-19 14:31:40 +0200

    feat(profile): add publication time field to user post details

--- lib/models/ui/user_post_details.dart
+++ lib/models/ui/user_post_details.dart
@@ -8,12 +8,14 @@ class UserPostDetails {
   final PostIdFirestore postId;
   final String title;
   final String description;
+  final DateTime publicationTime;
   // TODO add the location to be able to redirect to the map (see #184)
 
   const UserPostDetails({
     required this.postId,
     required this.title,
     required this.description,
+    required this.publicationTime,
   });
 
   @override
@@ -23,7 +25,8 @@ class UserPostDetails {
     return other is UserPostDetails &&
         other.postId == postId &&
         other.title == title &&
-        other.description == description;
+        other.description == description &&
+        other.publicationTime == publicationTime;
   }
 
   @override
@@ -32,6 +35,7 @@ class UserPostDetails {
       postId,
       title,
       description,
+      publicationTime,
     );
   }
 }
