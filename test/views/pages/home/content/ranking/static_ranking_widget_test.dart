26c5bbb2d3238231e450bcfae8c13e16a0eb5bec
CHOOSEIT <chooseitytt@gmail.com>
2024-05-19 16:04:57 +0200

    test(ranking_widget): wrap test in group and rename test

--- test/views/pages/home/content/ranking/static_ranking_widget_test.dart
+++ test/views/pages/home/content/ranking/static_ranking_widget_test.dart
@@ -5,35 +5,37 @@ import "package:proxima/views/pages/home/content/ranking/ranking_widget.dart";
 import "../../../../../mocks/providers/provider_homepage.dart";
 
 void main() {
-  testWidgets(
-    "Check that all the ranking elements are displayed correctly",
-    (tester) async {
-      await tester.pumpWidget(nonEmptyHomePageProvider);
-      await tester.pumpAndSettle();
+  group("Widgets display", () {
+    testWidgets(
+      "Display ranking elements",
+      (tester) async {
+        await tester.pumpWidget(nonEmptyHomePageProvider);
+        await tester.pumpAndSettle();
 
-      // Navigate to the ranking page
-      await tester.tap(find.text("Ranking"));
-      await tester.pumpAndSettle();
+        // Navigate to the ranking page
+        await tester.tap(find.text("Ranking"));
+        await tester.pumpAndSettle();
 
-      // Check that the ranking widget is displayed
-      final rankingWidget = find.byType(RankingWidget);
-      expect(rankingWidget, findsOneWidget);
+        // Check that the ranking widget is displayed
+        final rankingWidget = find.byType(RankingWidget);
+        expect(rankingWidget, findsOneWidget);
 
-      // Check that the ranking list is displayed
-      final rankingListWidget = find.byKey(RankingWidget.rankingListKey);
-      expect(rankingListWidget, findsOneWidget);
+        // Check that the ranking list is displayed
+        final rankingListWidget = find.byKey(RankingWidget.rankingListKey);
+        expect(rankingListWidget, findsOneWidget);
 
-      // Check that the user ranking card is displayed
-      final userRankingCard = find.byKey(RankingWidget.userRankingCardKey);
-      expect(userRankingCard, findsOneWidget);
+        // Check that the user ranking card is displayed
+        final userRankingCard = find.byKey(RankingWidget.userRankingCardKey);
+        expect(userRankingCard, findsOneWidget);
 
-      // Click on the user ranking card
-      await tester.tap(userRankingCard);
-      await tester.pumpAndSettle();
+        // Click on the user ranking card
+        await tester.tap(userRankingCard);
+        await tester.pumpAndSettle();
 
-      // Check that the profile page pop up is displayed
-      final profilePopUp = find.byType(UserProfilePopUp);
-      expect(profilePopUp, findsOneWidget);
-    },
-  );
+        // Check that the profile page pop up is displayed
+        final profilePopUp = find.byType(UserProfilePopUp);
+        expect(profilePopUp, findsOneWidget);
+      },
+    );
+  });
 }
