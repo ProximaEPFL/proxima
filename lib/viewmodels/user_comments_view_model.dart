8dabb917fa59363dbc08f98fc4c1785db7bf695d
Yoann Lafore <yoann.lafore@gmail.com>
2024-05-20 16:02:23 +0200

    fix(profile): use sorted instead of ..sort for consistency with user post viewmodel

--- lib/viewmodels/user_comments_view_model.dart
+++ lib/viewmodels/user_comments_view_model.dart
@@ -1,3 +1,4 @@
+import "package:collection/collection.dart";
 import "package:hooks_riverpod/hooks_riverpod.dart";
 import "package:proxima/models/database/comment/comment_id_firestore.dart";
 import "package:proxima/models/database/post/post_id_firestore.dart";
@@ -19,11 +20,10 @@ class UserCommentViewModel extends AutoDisposeAsyncNotifier<UserCommentsState> {
     final commentsFirestore = await userCommentRepository.getUserComments(user);
 
     // Sort the comments from latest to oldest
-    final sortedCommentsFirestore = commentsFirestore.toList()
-      ..sort(
-        (commentA, commentB) => commentB.data.publicationTime
-            .compareTo(commentA.data.publicationTime),
-      );
+    final sortedCommentsFirestore = commentsFirestore.toList().sorted(
+          (commentA, commentB) => commentB.data.publicationTime
+              .compareTo(commentA.data.publicationTime),
+        );
 
     final comments = sortedCommentsFirestore.map((comment) {
       final userComment = UserCommentDetails(
