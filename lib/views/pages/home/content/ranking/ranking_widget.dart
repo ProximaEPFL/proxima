e4914bb2e24753240284b1a85c1f63acde9c83a9
gruvw <gruvw.dev@gmail.com>
2024-05-20 19:16:16 +0200

    feat(ranking-vm): convert ranking widget to get a ranking details arg

--- lib/views/pages/home/content/ranking/ranking_widget.dart
+++ lib/views/pages/home/content/ranking/ranking_widget.dart
@@ -1,72 +1,37 @@
-import "package:collection/collection.dart";
 import "package:flutter/material.dart";
+import "package:hooks_riverpod/hooks_riverpod.dart";
 import "package:proxima/models/ui/ranking/ranking_details.dart";
-import "package:proxima/models/ui/ranking/ranking_element_details.dart";
+import "package:proxima/viewmodels/users_ranking_view_model.dart";
 import "package:proxima/views/pages/home/content/ranking/ranking_card.dart";
 import "package:proxima/views/pages/home/content/ranking/ranking_list.dart";
 
 /// A widget that displays the ranking widget.
-class RankingWidget extends StatelessWidget {
+class RankingWidget extends ConsumerWidget {
   static const rankingListKey = Key("rankingList");
   static const userRankingCardKey = Key("userRankingCard");
 
-  RankingWidget({super.key});
+  final RankingDetails ranking;
 
-  // Creating mock data for the ranking list
-  static const List<Map<String, int>> userList = [
-    {"user123": 9420},
-    {"johnDoe": 8370},
-    {"janeSmith": 6550},
-    {"coolGuy": 5280},
-    {"happyGal": 3000},
-    {"superman": 2310},
-    {"batman": 1530},
-    {"wonderWoman": 420},
-  ];
-
-  final RankingElementDetails userRankElementDetails =
-      const RankingElementDetails(
-    userDisplayName: "You",
-    userUserName: "U_You",
-    centauriPoints: 100,
-    userRank: null,
-  );
-
-  final rankElementDetailsList = userList
-      .mapIndexed(
-        (index, user) => RankingElementDetails(
-          userDisplayName: user.keys.first,
-          userUserName: "u_${user.keys.first}",
-          centauriPoints: user.values.first,
-          userRank: index + 1,
-        ),
-      )
-      .toList();
+  const RankingWidget({
+    super.key,
+    required this.ranking,
+  });
 
   @override
-  Widget build(BuildContext context) {
-    //TODO: Replace with view model RankingDetails getter logic
-    final rankingDetails = RankingDetails(
-      rankElementDetailsList: rankElementDetailsList,
-      userRankElementDetails: userRankElementDetails,
-    );
-
-    //TODO: Add Circular value waiting on data.
+  Widget build(BuildContext context, WidgetRef ref) {
     return Column(
       children: [
         Flexible(
           child: RankingList(
             key: rankingListKey,
-            rankingDetails: rankingDetails,
-            onRefresh: () async {
-              //TODO: Add refresh logic
-            },
+            rankingDetails: ranking,
+            onRefresh: ref.read(usersRankingViewModelProvider.notifier).refresh,
           ),
         ),
         const Divider(),
         RankingCard(
           key: userRankingCardKey,
-          rankingElementDetails: rankingDetails.userRankElementDetails,
+          rankingElementDetails: ranking.userRankElementDetails,
         ),
         const SizedBox(height: 5),
       ],
