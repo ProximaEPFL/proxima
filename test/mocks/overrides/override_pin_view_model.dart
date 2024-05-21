3624d2c9998e20d40ed749796b665870178ec44e
TheTexanCodeur <92913266+TheTexanCodeur@users.noreply.github.com>
2024-05-21 17:04:24 +0200

    test(pin viewmodel): added context to the override

--- test/mocks/overrides/override_pin_view_model.dart
+++ test/mocks/overrides/override_pin_view_model.dart
@@ -1,3 +1,4 @@
+import "package:flutter/material.dart";
 import "package:hooks_riverpod/hooks_riverpod.dart";
 import "package:proxima/models/ui/map_pin_details.dart";
 import "package:proxima/viewmodels/map/map_pin_view_model.dart";
@@ -8,10 +9,13 @@ import "../data/map_pin.dart";
 class MockPinViewModel extends AutoDisposeAsyncNotifier<List<MapPinDetails>>
     implements MapPinViewModel {
   final Future<List<MapPinDetails>> Function() _build;
+  final Function(BuildContext context) _setContext;
 
   MockPinViewModel({
     Future<List<MapPinDetails>> Function()? build,
-  }) : _build = build ?? (() async => List.empty());
+    Function(BuildContext)? setContext,
+  })  : _build = build ?? (() async => List.empty()),
+        _setContext = setContext ?? ((_) => {});
 
   @override
   Future<List<MapPinDetails>> build() => _build();
@@ -21,6 +25,12 @@ class MockPinViewModel extends AutoDisposeAsyncNotifier<List<MapPinDetails>>
     state = const AsyncValue.loading();
     state = await AsyncValue.guard(() => build());
   }
+
+  @override
+  void setContext(BuildContext context) => _setContext(context);
+
+  @override
+  BuildContext? context;
 }
 
 final mockPinViewModelOverride = mapPinViewModelProvider.overrideWith(
