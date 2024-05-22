59acca0bdf78dc1adef3b246b3824eb26b40cdd4
gruvw <gruvw.dev@gmail.com>
2024-05-22 08:33:52 +0200

    docs(ranking-vm): documentation of `RankingPage`

--- lib/views/pages/home/content/ranking/ranking_page.dart
+++ lib/views/pages/home/content/ranking/ranking_page.dart
@@ -4,6 +4,9 @@ import "package:proxima/viewmodels/users_ranking_view_model.dart";
 import "package:proxima/views/components/async/circular_value.dart";
 import "package:proxima/views/pages/home/content/ranking/components/ranking_widget.dart";
 
+/// The Ranking page home content that is accessible via the bottom
+/// navigation bar on the home screen.
+/// It displays a leaderboard of top centauri points users.
 class RankingPage extends ConsumerWidget {
   const RankingPage({super.key});
 
