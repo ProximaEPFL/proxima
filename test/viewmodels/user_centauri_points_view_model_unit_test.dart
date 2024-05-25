1e29b841089e1e2c31918e79175fce59c337acf7
CHOOSEIT <chooseitytt@gmail.com>
2024-05-25 16:22:53 +0200

    test(user_centauri_points_vm): add unit tests

---
+++ test/viewmodels/user_centauri_points_view_model_unit_test.dart
@@ -0,0 +1,63 @@
+import "package:fake_cloud_firestore/fake_cloud_firestore.dart";
+import "package:flutter_test/flutter_test.dart";
+import "package:hooks_riverpod/hooks_riverpod.dart";
+import "package:proxima/models/database/user/user_firestore.dart";
+import "package:proxima/services/database/firestore_service.dart";
+import "package:proxima/services/database/user_repository_service.dart";
+import "package:proxima/viewmodels/user_centauri_points_view_model.dart";
+
+import "../mocks/data/firestore_user.dart";
+
+void main() {
+  group("User Centauri points ViewModel unit testing", () {
+    late FakeFirebaseFirestore fakeFireStore;
+    late UserRepositoryService userRepo;
+
+    late ProviderContainer container;
+
+    late List<UserFirestore> availableUsers;
+
+    setUp(() async {
+      fakeFireStore = FakeFirebaseFirestore();
+
+      userRepo = UserRepositoryService(
+        firestore: fakeFireStore,
+      );
+
+      availableUsers = FirestoreUserGenerator.generateUserFirestore(10);
+      await setUsersFirestore(fakeFireStore, availableUsers);
+
+      //Prepare the list of providers to override.
+      container = ProviderContainer(
+        overrides: [
+          firestoreProvider.overrideWithValue(fakeFireStore),
+          userRepositoryServiceProvider.overrideWithValue(userRepo),
+        ],
+      );
+    });
+
+    group("User Centauri points unit testing", () {
+      test("Find centauri points given null user id", () async {
+        final centauriPoints = await container.read(
+          userCentauriPointsViewModelProvider(null).future,
+        );
+        expect(
+          centauriPoints,
+          isNull,
+        );
+      });
+
+      test("Find centauri points given user id in available users", () async {
+        for (final user in availableUsers) {
+          final centauriPoints = await container.read(
+            userCentauriPointsViewModelProvider(user.uid).future,
+          );
+          expect(
+            centauriPoints,
+            user.data.centauriPoints,
+          );
+        }
+      });
+    });
+  });
+}
