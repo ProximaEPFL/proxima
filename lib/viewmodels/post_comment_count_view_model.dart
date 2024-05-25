feff9b3e4fe15aebaa7d361c8e4eea5b00225c2e
Joachim Favre <joachim@favre.xyz>
2024-05-25 19:29:57 +0200

    docs(comment-count): add documentation to view-model

--- lib/viewmodels/post_comment_count_view_model.dart
+++ lib/viewmodels/post_comment_count_view_model.dart
@@ -2,6 +2,10 @@ import "package:hooks_riverpod/hooks_riverpod.dart";
 import "package:proxima/models/database/post/post_id_firestore.dart";
 import "package:proxima/services/database/post_repository_service.dart";
 
+/// This view model is used to keep in memory the number of comments of a post.
+/// It is refreshed every time the post comment list is refreshed, to stay consistent
+/// with it. Since it is also auto-dispose, as soon as we refresh the posts list,
+/// it will be disposed and recreated with the correct fetched data.
 class PostCommentCountViewModel
     extends AutoDisposeFamilyAsyncNotifier<int, PostIdFirestore> {
   @override
@@ -11,6 +15,8 @@ class PostCommentCountViewModel
     return post.data.commentCount;
   }
 
+  /// Set the count of comments of the post to [count]. This updates
+  /// the state without needing to fetch the data from the database.
   Future<void> setCount(int count) async {
     // We wait for the previous future to avoid race conditions with
     // the build function.
