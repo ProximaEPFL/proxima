e5c2f6c9126b6c78bfa206d04e40dc99a7a1575c
Joachim Favre <joachim@favre.xyz>
2024-05-25 19:48:35 +0200

    docs(comment-count): improve documentation about auto-dispose

--- lib/viewmodels/post_comment_count_view_model.dart
+++ lib/viewmodels/post_comment_count_view_model.dart
@@ -4,8 +4,11 @@ import "package:proxima/services/database/post_repository_service.dart";
 
 /// This view model is used to keep in memory the number of comments of a post.
 /// It is refreshed every time the post comment list is refreshed, to stay consistent
-/// with it. Since it is also auto-dispose, as soon as we refresh the posts list,
-/// it will be disposed and recreated with the correct fetched data.
+/// with it. It is also refreshed when a comment is deleted.
+/// The fact that it is auto-dispose allows to keep the memory usage low, keeping only
+/// the count of the comments of the posts that are currently displayed on the screen.
+/// This may moreover help mitigate some reloading bugs, since it suffices to scroll
+/// the post out of the screen to dispose the view model and thus the count of the comments.
 class PostCommentCountViewModel
     extends AutoDisposeFamilyAsyncNotifier<int, PostIdFirestore> {
   @override
