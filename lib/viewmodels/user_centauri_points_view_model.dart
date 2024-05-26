7850437df297d31ee796897e115b38325b7682e3
CHOOSEIT <chooseitytt@gmail.com>
2024-05-26 19:13:22 +0200

    feat(user_centauri_points_vm): remove rethrow error

--- lib/viewmodels/user_centauri_points_view_model.dart
+++ lib/viewmodels/user_centauri_points_view_model.dart
@@ -3,7 +3,6 @@ import "dart:async";
 import "package:hooks_riverpod/hooks_riverpod.dart";
 import "package:proxima/models/database/user/user_id_firestore.dart";
 import "package:proxima/services/database/user_repository_service.dart";
-import "package:proxima/views/components/async/circular_value.dart";
 
 /// A view model that provides the number of centauri points of a user
 /// given its user id. Allow a null [arg] (user id) to return null, which
@@ -14,22 +13,16 @@ class UserCentauriPointsViewModel
 
   @override
   Future<int?> build(UserIdFirestore? arg) async {
-    try {
-      final userDataBase = ref.watch(userRepositoryServiceProvider);
+    final userDataBase = ref.watch(userRepositoryServiceProvider);
 
-      if (arg == null) {
-        return null;
-      }
-      final userID = arg;
-
-      final user = await userDataBase.getUser(userID);
-      final userCentauri = user.data.centauriPoints;
-      return userCentauri;
-    } catch (e) {
-      return throw Exception(
-        "${CircularValue.debugErrorTag} User avatar color error: $e",
-      );
+    if (arg == null) {
+      return null;
     }
+    final userID = arg;
+
+    final user = await userDataBase.getUser(userID);
+    final userCentauri = user.data.centauriPoints;
+    return userCentauri;
   }
 
   /// Refresh the number of centauri points of the user.
