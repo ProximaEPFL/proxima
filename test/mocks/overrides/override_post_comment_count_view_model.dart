9067b9cc6bc6c63c119e24ec23c1acf83a3152b6
Joachim Favre <joachim@favre.xyz>
2024-05-26 16:31:09 +0200

    docs: add documentation to MockPostCommentCountViewModel

--- test/mocks/overrides/override_post_comment_count_view_model.dart
+++ test/mocks/overrides/override_post_comment_count_view_model.dart
@@ -2,11 +2,15 @@ import "package:hooks_riverpod/hooks_riverpod.dart";
 import "package:proxima/models/database/post/post_id_firestore.dart";
 import "package:proxima/viewmodels/post_comment_count_view_model.dart";
 
+/// A mock implementation of the [PostCommentCountViewModel] class.
+/// Its state is always the same and can be set in the constructor.
 class MockPostCommentCountViewModel
     extends AutoDisposeFamilyAsyncNotifier<int, PostIdFirestore>
     implements PostCommentCountViewModel {
   final int count;
 
+  /// Creates a new [MockPostCommentCountViewModel] with the given [count],
+  /// which is the value that will always be returned by this view-model.
   MockPostCommentCountViewModel({this.count = 0});
 
   @override
