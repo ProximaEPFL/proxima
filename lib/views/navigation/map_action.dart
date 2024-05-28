2b617aaba8e12834485e3aa3a6db150509c9855f
Alberts Reisons <alberts.reisons@epfl.ch>
2024-05-28 09:59:17 +0200

    feat(navigation-to-map): navigate to map in action

--- lib/views/navigation/map_action.dart
+++ lib/views/navigation/map_action.dart
@@ -1,17 +1,25 @@
 import "package:flutter/material.dart";
+import "package:hooks_riverpod/hooks_riverpod.dart";
+import "package:proxima/viewmodels/option_selection/selected_page_view_model.dart";
 
-class MapAction extends StatelessWidget {
+import "bottom_navigation_bar/navigation_bar_routes.dart";
+
+class MapAction extends ConsumerWidget {
   final int depth;
 
   const MapAction({super.key, required this.depth});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return IconButton(
       onPressed: () {
         for (var i = 0; i < depth; i++) {
           Navigator.pop(context);
         }
+        ref.watch(selectedPageViewModelProvider.notifier).navigate(
+              NavigationBarRoutes.map,
+              context,
+            );
       },
       icon: const Icon(Icons.map),
     );
