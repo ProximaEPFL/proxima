e6b1b20a4a1a8952549a3d5651c31ee0043d9208
Alberts Reisons <alberts.reisons@epfl.ch>
2024-05-29 16:17:04 +0200

    test(navigation-to-map): test map action

---
+++ test/views/navigation/map_action_test.dart
@@ -0,0 +1,44 @@
+import "package:flutter/material.dart";
+import "package:flutter_test/flutter_test.dart";
+import "package:hooks_riverpod/hooks_riverpod.dart";
+import "package:proxima/models/ui/selected_page_details.dart";
+import "package:proxima/viewmodels/option_selection/selected_page_view_model.dart";
+import "package:proxima/views/components/options/map/map_selection_options.dart";
+import "package:proxima/views/navigation/bottom_navigation_bar/navigation_bar_routes.dart";
+import "package:proxima/views/navigation/map_action.dart";
+
+void main() {
+  late SelectedPageViewModel selectedPageViewModel;
+  late ProviderScope mapActionWidget;
+
+  setUp(() {
+    selectedPageViewModel = SelectedPageViewModel();
+
+    mapActionWidget = ProviderScope(
+      overrides: [
+        selectedPageViewModelProvider.overrideWith(() => selectedPageViewModel),
+      ],
+      child: const MaterialApp(
+        home: MapAction(
+          depth: 0,
+          mapOption: MapSelectionOptions.myPosts,
+        ),
+      ),
+    );
+  });
+
+  testWidgets("MapAction shows and navigates correctly",
+      (WidgetTester tester) async {
+    await tester.pumpWidget(mapActionWidget);
+
+    expect(find.byKey(MapAction.mapActionKey), findsOneWidget);
+
+    expect(find.byIcon(Icons.place), findsOneWidget);
+
+    await tester.tap(find.byKey(MapAction.mapActionKey));
+    await tester.pumpAndSettle();
+
+    expect(selectedPageViewModel.state,
+        const SelectedPageDetails(route: NavigationBarRoutes.map));
+  });
+}
